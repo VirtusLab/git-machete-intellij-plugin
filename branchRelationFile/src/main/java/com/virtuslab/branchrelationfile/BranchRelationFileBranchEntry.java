89bdcab1cbae2e121835ac7e328bbd290faf7c18
Michał Piotrowski <mpiotrowski@virtuslab.com>
2020-01-14 11:57:58 +0100

    Sliding out branches in memory structure

--- branchRelationFile/src/main/java/com/virtuslab/branchrelationfile/BranchRelationFileBranchEntry.java
+++ branchRelationFile/src/main/java/com/virtuslab/branchrelationfile/BranchRelationFileBranchEntry.java
@@ -16,4 +16,18 @@ public class BranchRelationFileBranchEntry {
   void addSubbranch(BranchRelationFileBranchEntry subbranch) {
     subbranches.add(subbranch);
   }
+
+  public void slideOut() throws BranchRelationFileException {
+    if (upstream.isEmpty()) throw new BranchRelationFileException("Can not slide out root branch");
+
+    var upBranch = upstream.get();
+    int indexOfThatBranch = upBranch.subbranches.indexOf(this);
+
+    for (var childBranch : subbranches) {
+      childBranch.upstream = upstream;
+      upBranch.subbranches.add(indexOfThatBranch, childBranch);
+    }
+
+    upBranch.subbranches.remove(this);
+  }
 }
--- gitMacheteTest/src/main/java/com/virtuslab/gitmachete/test/Main.java
+++ gitMacheteTest/src/main/java/com/virtuslab/gitmachete/test/Main.java
@@ -1,16 +1,13 @@
 package com.virtuslab.gitmachete.test;
 
-import com.virtuslab.gitmachete.backendroot.GitFactoryModule;
-import com.virtuslab.gitmachete.gitmacheteapi.GitMacheteException;
-import com.virtuslab.gitmachete.gitmacheteapi.GitMacheteRepositoryFactory;
-import com.virtuslab.gitmachete.gitmacheteapi.IGitMacheteRepository;
+import com.virtuslab.branchrelationfile.BranchRelationFile;
+import com.virtuslab.branchrelationfile.BranchRelationFileBranchEntry;
 import java.nio.file.Paths;
-import java.util.Optional;
+import java.util.List;
 
 public class Main {
   public static void main(String[] argv) throws Exception {
-    IGitMacheteRepository repo = null;
-    IGitMacheteRepository repo2 = null;
+    /*IGitMacheteRepository repo = null;
 
     try {
       repo =
@@ -18,35 +15,28 @@ public class Main {
               .getInstance(GitMacheteRepositoryFactory.class)
               .create(
                   Paths.get(
-                      /*"/tmp/machete-tests/machete-sandbox"*/ System.getProperty("user.home"),
-                      "submodule-test"),
+                      System.getProperty("user.home"),
+                      "machete-sandbox"),
                   Optional.empty());
     } catch (GitMacheteException e) {
       System.err.println(e.getMessage());
       e.printStackTrace();
-    }
+    }*/
 
-    var subs = repo.getSubmodules();
+    var macheteFile =
+        new BranchRelationFile(
+            Paths.get(System.getProperty("user.home"), "machete-sandbox", ".git", "machete"));
 
-    subs.forEach(m -> System.out.println(m.getName() + " " + m.getPath()));
+    printBranches(macheteFile.getRootBranches(), 0);
 
-    try {
-      repo2 =
-          GitFactoryModule.getInjector()
-              .getInstance(GitMacheteRepositoryFactory.class)
-              .create(subs.get(0).getPath(), Optional.of(subs.get(0).getName()));
-    } catch (GitMacheteException e) {
-      System.err.println(e.getMessage());
-      e.printStackTrace();
-    }
+    macheteFile.getRootBranches().get(0).getSubbranches().get(0).getSubbranches().get(0).slideOut();
+    System.out.println();
 
-    System.out.println(repo2.getRepositoryName());
+    printBranches(macheteFile.getRootBranches(), 0);
 
-    System.out.println(repo2.getSubmodules().size());
+    // System.out.println(repo2.getCurrentBranch().get().getName());
 
-    System.out.println(repo2.getCurrentBranch().get().getName());
-
-    repo2.getRootBranches().forEach(m -> System.out.println(m.getName()));
+    // repo2.getRootBranches().forEach(m -> System.out.println(m.getName()));
 
     /*var branches = repo.getRootBranches();
 
@@ -62,5 +52,13 @@ public class Main {
     }*/
 
     // System.out.println(repo.getSubmoduleRepositories());
+  }
+
+  private static void printBranches(List<BranchRelationFileBranchEntry> branches, int level) {
+    for (var branch : branches) {
+      System.out.println(
+          "\t".repeat(level) + branch.getName() + " # " + branch.getCustomAnnotation());
+      printBranches(branch.getSubbranches(), level + 1);
+    }
   }
 }
