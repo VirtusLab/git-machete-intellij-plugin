a2ebcf26d4f06aafe7846bb0c6447a7e1f36c861
Michał Piotrowski <mpiotrowski@virtuslab.com>
2020-02-18 20:11:26 +0100

    Second attempts

--- intellijPlugin/src/main/java/com/virtuslab/gitmachete/actions/CheckoutBranchAction.java
+++ intellijPlugin/src/main/java/com/virtuslab/gitmachete/actions/CheckoutBranchAction.java
@@ -6,6 +6,7 @@ import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import git4idea.GitUtil;
 import git4idea.branch.GitBranchUiHandlerImpl;
 import git4idea.branch.GitBranchWorker;
@@ -14,9 +15,12 @@ import git4idea.repo.GitRepository;
 import java.util.Iterator;
 import java.util.List;
 import javax.annotation.Nonnull;
+import lombok.Setter;
 
 public class CheckoutBranchAction extends AnAction {
   private static final Logger LOG = Logger.getInstance(CheckoutBranchAction.class);
+
+  @Setter private static String nameOfBranchToCheckout;
 
   public CheckoutBranchAction() {}
 
@@ -28,18 +32,25 @@ public class CheckoutBranchAction extends AnAction {
 
   @Override
   public void actionPerformed(@Nonnull AnActionEvent anActionEvent) {
+    if (nameOfBranchToCheckout == null) {
+      LOG.error("Branch to checkout was not given");
+      Messages.showErrorDialog(
+          "Internal plugin error occurred while checking out branch", "Ooops...");
+      return;
+    }
+
     Project project = anActionEvent.getProject();
     assert project != null;
     GitRepository repository = getRepository(project);
 
-    new Task.Backgroundable(project, "Rebasing") {
+    new Task.Backgroundable(project, "Checking out") {
       @Override
       public void run(@Nonnull ProgressIndicator indicator) {
         new GitBranchWorker(
                 project,
                 Git.getInstance(),
                 new GitBranchUiHandlerImpl(project, Git.getInstance(), indicator))
-            .checkout("call-ws", false, List.of(repository));
+            .checkout(nameOfBranchToCheckout, false, List.of(repository));
       }
 
       /* todo on success
@@ -48,16 +59,6 @@ public class CheckoutBranchAction extends AnAction {
           (eg. commits may get squashed so the graph structure changes)
       */
     }.queue();
-
-    /*int row = rowAtPoint(e.getPoint());
-
-    int col = columnAtPoint(e.getPoint());
-
-              /*JOptionPane.showMessageDialog(
-                      null, "Value in the cell clicked :" + " " + getValueAt(row, col).toString());
-
-    JOptionPane.showMessageDialog(null, "bbbbb");*/
-
   }
 
   protected GitRepository getRepository(Project project) {
--- intellijPlugin/src/main/java/com/virtuslab/gitmachete/ui/table/GitMacheteGraphTable.java
+++ intellijPlugin/src/main/java/com/virtuslab/gitmachete/ui/table/GitMacheteGraphTable.java
@@ -2,24 +2,31 @@ package com.virtuslab.gitmachete.ui.table;
 
 import com.intellij.ide.DataManager;
 import com.intellij.ide.ui.customization.CustomizationUtil;
-import com.intellij.openapi.actionSystem.DataConstants;
-import com.intellij.openapi.actionSystem.DataContext;
-import com.intellij.openapi.actionSystem.DataProvider;
-import com.intellij.openapi.project.Project;
+import com.intellij.openapi.actionSystem.ActionGroup;
+import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionPlaces;
+import com.intellij.openapi.actionSystem.ActionPopupMenu;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.Presentation;
+import com.intellij.openapi.diagnostic.Logger;
 import com.intellij.ui.ScrollingUtil;
 import com.intellij.ui.table.JBTable;
 import com.intellij.util.ui.JBUI;
 import com.intellij.vcs.log.paint.GraphCellPainter;
 import com.intellij.vcs.log.paint.SimpleGraphCellPainter;
+import com.virtuslab.gitmachete.actions.CheckoutBranchAction;
 import com.virtuslab.gitmachete.graph.SyncToParentStatusEdgeColorGenerator;
 import com.virtuslab.gitmachete.ui.cell.BranchOrCommitCell;
 import com.virtuslab.gitmachete.ui.cell.BranchOrCommitCellRenderer;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import javax.annotation.Nonnull;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
+import javax.swing.SwingUtilities;
 
 /* todo: consider applying SpeedSearch for branches and commits */
 public class GitMacheteGraphTable extends JBTable {
+  private static final Logger LOG = Logger.getInstance(GitMacheteGraphTable.class);
 
   private static final String GIT_MACHETE_TEXT = "Git Machete Status";
 
@@ -52,34 +59,9 @@ public class GitMacheteGraphTable extends JBTable {
 
     ScrollingUtil.installActions(this, false);
 
-    CustomizationUtil.installPopupHandler(this, "GitMachete.ContextMenu", "GitMachete.ContextMenu");
+    addMouseListener(new GitMacheteGraphTableMouseAdapter(this));
 
-    /*addMouseListener(
-    new java.awt.event.MouseAdapter() {
-
-      public void mouseClicked(java.awt.event.MouseEvent e) {
-
-        if (e.getClickCount() == 2 && !e.isConsumed()) {
-          e.consume();
-
-          int row = rowAtPoint(e.getPoint());
-
-          int col = columnAtPoint(e.getPoint());
-
-
-
-          ActionPopupMenu apm =
-                  ActionManager.getInstance()
-                          .createActionPopupMenu(
-                                  "GitMachete.ContextMenu",
-                                  (ActionGroup) ActionManager.getInstance().getAction("GitMachete.ContextMenu"));
-          apm.getComponent().show(e.getComponent(), e.getX(), e.getY());
-
-          //System.out.println(
-                  //"Value in the cell clicked :" + " " + getValueAt(row, col).toString());
-        }
-      }
-    });*/
+    //CustomizationUtil.installPopupHandler(this, "GitMachete.ContextMenu", "GitMachete.ContextMenu");
   }
 
   private void initColumns() {
@@ -95,14 +77,28 @@ public class GitMacheteGraphTable extends JBTable {
     return (GraphTableModel) super.getModel();
   }
 
-  /*@Nullable
-  @Override
-  public Object getData(@NotNull String dataId) {
-    if (dataId.equals("project")) {
-      DataContext dataContext = DataManager.getInstance().getDataContext();
-      Project project = (Project) dataContext.getData(DataConstants.PROJECT);
-      return project;
+  protected class GitMacheteGraphTableMouseAdapter extends MouseAdapter {
+    private final GitMacheteGraphTable graphTable;
+
+    public GitMacheteGraphTableMouseAdapter(GitMacheteGraphTable graphTable) {
+      this.graphTable = graphTable;
     }
-    return dataId;
-  }*/
+
+    public void mouseClicked(MouseEvent e) {
+      System.out.println("CLICK!");
+      if (SwingUtilities.isRightMouseButton(e)) {
+        Point point = e.getPoint();
+        String branchName = getValueAt(rowAtPoint(point), columnAtPoint(point)).toString();
+        System.out.println("BRANCH NAME: "+branchName + " r: "+rowAtPoint(point)+" c: "+columnAtPoint(point));
+        CheckoutBranchAction.setNameOfBranchToCheckout(branchName);
+        ActionPopupMenu actionPopupMenu =
+                ActionManager.getInstance().createActionPopupMenu(ActionPlaces.UNKNOWN,(ActionGroup)ActionManager.getInstance().getAction("GitMachete.ContextMenu")) ;
+        actionPopupMenu.getComponent().show(graphTable, (int)point.getX(), (int)point.getY());
+        /*ActionManager am = ActionManager.getInstance();
+        am.getAction("GitMachete.CheckoutBranchAction").actionPerformed(new AnActionEvent(null, DataManager.getInstance().getDataContext(),
+                ActionPlaces.UNKNOWN, new Presentation(),
+                ActionManager.getInstance(), 0));*/
+      }
+    }
+  }
 }
