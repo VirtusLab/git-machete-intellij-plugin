3b37580160666df99b0df162180939e2f9f69ca3
Mikołaj Kondratek <mkondratek@virtuslab.com>
2020-02-20 17:34:07 +0100

    Use api and implementation configurations

--- gitMacheteApi/src/main/java/com/virtuslab/gitmachete/gitmacheteapi/NullRepository.java
+++ gitMacheteApi/src/main/java/com/virtuslab/gitmachete/gitmacheteapi/NullRepository.java
@@ -1,17 +1,19 @@
-package com.virtuslab.gitmachete.graph.repositorygraph.data;
+package com.virtuslab.gitmachete.gitmacheteapi;
 
 import com.virtuslab.branchrelationfile.api.IBranchRelationFile;
-import com.virtuslab.gitmachete.gitmacheteapi.IGitMacheteBranch;
-import com.virtuslab.gitmachete.gitmacheteapi.IGitMacheteRepository;
-import com.virtuslab.gitmachete.gitmacheteapi.IGitMacheteSubmoduleEntry;
+import com.virtuslab.gitmachete.api.GitMacheteException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import javax.annotation.Nonnull;
-import lombok.Getter;
 
 public final class NullRepository implements IGitMacheteRepository {
-  @Getter @Nonnull private static final NullRepository instance = new NullRepository();
+  private static NullRepository instance = new NullRepository();
+
+  private NullRepository() {}
+
+  public static IGitMacheteRepository getInstance() {
+    return instance;
+  }
 
   @Override
   public List<IGitMacheteBranch> getRootBranches() {
