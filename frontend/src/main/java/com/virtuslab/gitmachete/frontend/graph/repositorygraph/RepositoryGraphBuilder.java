1ab507b9d0066f53decae7e48ee7f885dae47511
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-03-21 16:40:00 +0100

    Simplify import order

--- frontend/src/main/java/com/virtuslab/gitmachete/frontend/graph/repositorygraph/RepositoryGraphBuilder.java
+++ frontend/src/main/java/com/virtuslab/gitmachete/frontend/graph/repositorygraph/RepositoryGraphBuilder.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.intellij.openapi.diagnostic.Logger;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 import org.checkerframework.checker.nullness.qual.Nullable;
