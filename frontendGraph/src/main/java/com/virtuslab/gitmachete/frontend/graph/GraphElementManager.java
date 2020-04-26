8baa5efefb8e571a3d172057bbb0182cd88d13c5
Mikołaj Kondratek <mkondratek@virtuslab.com>
2020-04-26 12:46:52 +0200

    [frontendGraph] Remove graphElementComparator

--- frontendGraph/src/main/java/com/virtuslab/gitmachete/frontend/graph/GraphElementManager.java
+++ frontendGraph/src/main/java/com/virtuslab/gitmachete/frontend/graph/GraphElementManager.java
@@ -1,9 +1,5 @@
 package com.virtuslab.gitmachete.frontend.graph;
 
-import java.util.Comparator;
-
-import com.intellij.vcs.log.graph.api.elements.GraphElement;
-import com.intellij.vcs.log.graph.impl.print.GraphElementComparatorByLayoutIndex;
 import lombok.Getter;
 
 import com.virtuslab.gitmachete.frontend.graph.api.IGraphElement;
@@ -12,12 +8,10 @@ import com.virtuslab.gitmachete.frontend.graph.repository.RepositoryGraph;
 
 public class GraphElementManager {
   @Getter
-  private final Comparator<GraphElement> graphElementComparator;
   private final ColorGetterByLayoutIndex colorGetterByLayoutIndex;
 
   public GraphElementManager(RepositoryGraph repositoryGraph) {
     colorGetterByLayoutIndex = new ColorGetterByLayoutIndex(repositoryGraph);
-    graphElementComparator = new GraphElementComparatorByLayoutIndex(repositoryGraph::getNodeId).reversed();
   }
 
   public int getColorId(IGraphElement element) {
