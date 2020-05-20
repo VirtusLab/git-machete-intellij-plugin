7e7c9ec3c22bcfe43c2d8dd6610a7c0d1b7e97d1
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-05-20 18:33:51 +0200

    Add support for machete-status-branch hook - 1st round of fixes

--- testCommon/src/test/resources/common.sh
+++ testCommon/src/test/resources/common.sh
@@ -8,54 +8,30 @@ EOF
 )
 
 newrepo() {
-  if (( $# < 2 ))
-  then
-    echo "newrepo() needs at least 2 parameters, $# was given"
+  if (( $# < 2 )); then
+    echo "${FUNCNAME[0]} needs at least 2 parameters, $# was given"
     exit 100
   fi
-  path=$1
-  dir=$2
+  local path=$1
+  local dir=$2
   mkdir -p $path/$dir
   cd $path/$dir
-  opt=${3-}
+  local opt=${3-}
   git init $opt
   mkdir -p .git/hooks/
-  hook_path=.git/hooks/machete-status-branch
+  local hook_path=.git/hooks/machete-status-branch
   echo "$status_branch_hook" > $hook_path
   chmod +x $hook_path
 }
 
-newb() {
-  if (( $# != 1 ))
-  then
-    echo "newb() needs 1 parameter, $# was given"
-    exit 100
-  fi
-  git checkout -b $1
-}
-
-cmt() {
-  if (( $# < 1 ))
-  then
-    echo "cmt() needs at least 1 parameters, $# was given"
-    exit 100
-  fi
-  b=$(git symbolic-ref --short HEAD)
-  f=${b/\//-}-${1}-${2-}.txt
-  touch $f
-  git add $f
-  git commit -m "$*"
-}
-
 clone() {
-  if (( $# != 3 ))
-  then
-    echo "clone() needs 3 parameters, $# was given"
+  if (( $# != 3 )); then
+    echo "${FUNCNAME[0]} needs 3 parameters, $# was given"
     exit 100
   fi
-  path=$1
-  remote=$2
-  destinationDir=$3
+  local path=$1
+  local remote=$2
+  local destinationDir=$3
   cd $path
   git clone $remote $destinationDir
   cd $destinationDir
@@ -64,4 +40,29 @@ clone() {
 gituserdata() {
   git config --local user.email "circleci@example.com"
   git config --local user.name "CircleCI"
+}
+
+newb() {
+  if (( $# != 1 )); then
+    echo "${FUNCNAME[0]} needs 1 parameter, $# was given"
+    exit 100
+  fi
+  git checkout -b $1
+}
+
+cmt() {
+  if (( $# < 1 )); then
+    echo "${FUNCNAME[0]} needs at least 1 parameter, $# was given"
+    exit 100
+  fi
+  local b=$(git symbolic-ref --short HEAD)
+  local f=${b/\//-}-${1}-${2-}.txt
+  touch $f
+  git add $f
+  git commit -m "$*"
+}
+
+push() {
+  local b=$(git symbolic-ref --short HEAD)
+  git push -u ${1-origin} $b
 }
