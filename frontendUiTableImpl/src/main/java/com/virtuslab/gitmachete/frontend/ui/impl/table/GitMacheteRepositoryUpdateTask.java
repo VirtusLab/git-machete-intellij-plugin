b1cb96fda731a40319b12b31d3c830bb0268d9a9
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-05-27 15:34:15 +0200

    Decouple GitMacheteRepositoryUpdateTask from GitMacheteGraphTable and frontendUiApi from backendApi

--- frontendUiTableImpl/src/main/java/com/virtuslab/gitmachete/frontend/ui/impl/table/GitMacheteRepositoryUpdateTask.java
+++ frontendUiTableImpl/src/main/java/com/virtuslab/gitmachete/frontend/ui/impl/table/GitMacheteRepositoryUpdateTask.java
@@ -7,6 +7,7 @@ import static com.virtuslab.gitmachete.frontend.vfsutils.GitVfsUtils.getMainDire
 
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.function.Consumer;
 
 import com.intellij.openapi.progress.ProgressIndicator;
 import com.intellij.openapi.progress.Task;
@@ -17,6 +18,7 @@ import git4idea.repo.GitRepository;
 import io.vavr.control.Option;
 import io.vavr.control.Try;
 import lombok.CustomLog;
+import org.checkerframework.checker.guieffect.qual.UI;
 import org.checkerframework.checker.index.qual.Positive;
 
 import com.virtuslab.binding.RuntimeBinding;
@@ -26,55 +28,44 @@ import com.virtuslab.branchlayout.api.manager.IBranchLayoutReader;
 import com.virtuslab.gitmachete.backend.api.IGitMacheteRepository;
 import com.virtuslab.gitmachete.backend.api.IGitMacheteRepositoryFactory;
 import com.virtuslab.gitmachete.backend.api.MacheteFileReaderException;
-import com.virtuslab.gitmachete.frontend.ui.api.table.BaseGraphTable;
 import com.virtuslab.logger.IntelliJLoggingUtils;
 
 @CustomLog
 public final class GitMacheteRepositoryUpdateTask extends Task.Backgroundable {
 
-  private final BaseGraphTable graphTable;
-  private final Project project;
   private final GitRepository gitRepository;
   private final IBranchLayoutReader branchLayoutReader;
+  private final @UI Consumer<Option<IGitMacheteRepository>> doOnUIThreadWhenDone;
 
   private final IGitMacheteRepositoryFactory gitMacheteRepositoryFactory;
 
-  private GitMacheteRepositoryUpdateTask(BaseGraphTable graphTable, Project project, GitRepository gitRepository,
-      IBranchLayoutReader branchLayoutReader) {
+  public GitMacheteRepositoryUpdateTask(
+      Project project,
+      GitRepository gitRepository,
+      IBranchLayoutReader branchLayoutReader,
+      @UI Consumer<Option<IGitMacheteRepository>> doOnUIThreadWhenDone) {
+    // Quasi-title capitalization intended since we always write "Git Machete" with initial caps.
     super(project, "Updating Git Machete repository");
 
-    this.graphTable = graphTable;
-    this.project = project;
     this.gitRepository = gitRepository;
     this.branchLayoutReader = branchLayoutReader;
+    this.doOnUIThreadWhenDone = doOnUIThreadWhenDone;
 
     this.gitMacheteRepositoryFactory = RuntimeBinding.instantiateSoleImplementingClass(IGitMacheteRepositoryFactory.class);
   }
 
-  public static GitMacheteRepositoryUpdateTask of(BaseGraphTable graphTable, Project project, GitRepository gitRepository,
-      IBranchLayoutReader branchLayoutReader) {
-    return new GitMacheteRepositoryUpdateTask(graphTable, project, gitRepository, branchLayoutReader);
-  }
-
   @Override
   public void run(ProgressIndicator indicator) {
-    // We can't queue repository update (onto a non-UI thread) and graph table refresh (onto the UI thread) separately
+    // We can't queue repository update (onto a non-UI thread) and `doOnUIThreadWhenDone` (onto the UI thread) separately
     // since those two actions happen on two separate threads
-    // and graph table refresh can only start once repository update is complete.
+    // and `doOnUIThreadWhenDone` can only start once repository update is complete.
 
     // Thus, we synchronously run repository update first...
     Option<IGitMacheteRepository> gitMacheteRepository = updateRepository();
 
-    // ... and only once it completes, we queue graph table update onto the UI thread.
+    // ... and only once it completes, we queue `doOnUIThreadWhenDone` onto the UI thread.
     LOG.debug("Queuing graph table refresh onto the UI thread");
-    // A bit of a shortcut: we're accessing filesystem even though we may be on UI thread here;
-    // this shouldn't ever be a heavyweight operation, however.
-    Path macheteFilePath = getMacheteFilePath(gitRepository);
-    boolean isMacheteFilePresent = Files.isRegularFile(macheteFilePath);
-
-    GuiUtils.invokeLaterIfNeeded(
-        () -> graphTable.refreshModel(gitMacheteRepository.getOrNull(), macheteFilePath, isMacheteFilePresent),
-        NON_MODAL);
+    GuiUtils.invokeLaterIfNeeded(() -> doOnUIThreadWhenDone.accept(gitMacheteRepository), NON_MODAL);
   }
 
   /**
@@ -97,9 +88,9 @@ public final class GitMacheteRepositoryUpdateTask extends Task.Backgroundable {
       return Try.of(() -> {
         IBranchLayout branchLayout = createBranchLayout(macheteFilePath);
         return gitMacheteRepositoryFactory.create(mainDirectoryPath, gitDirectoryPath, branchLayout);
-      }).onFailure(this::handleUpdateRepositoryExceptions).toOption();
+      }).onFailure(this::handleUpdateRepositoryException).toOption();
     } else {
-      LOG.debug("Machete file is absent. Setting repository reference to null");
+      LOG.debug("Machete file is absent");
       return Option.none();
     }
   }
@@ -113,7 +104,7 @@ public final class GitMacheteRepositoryUpdateTask extends Task.Backgroundable {
         });
   }
 
-  private void handleUpdateRepositoryExceptions(Throwable t) {
+  private void handleUpdateRepositoryException(Throwable t) {
     LOG.error("Unable to create Git Machete repository", t);
 
     // Getting the innermost exception since it's usually the primary cause that gives most valuable message
@@ -123,7 +114,7 @@ public final class GitMacheteRepositoryUpdateTask extends Task.Backgroundable {
     }
     String exceptionMessage = cause.getMessage();
 
-    VcsNotifier.getInstance(project).notifyError("Repository instantiation failed",
+    VcsNotifier.getInstance(getProject()).notifyError("Repository instantiation failed",
         exceptionMessage != null ? exceptionMessage : "");
 
     IntelliJLoggingUtils.showErrorDialog(exceptionMessage != null
