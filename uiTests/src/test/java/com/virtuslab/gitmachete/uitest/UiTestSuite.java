f4528f6463b602ce4796aa4f5d60455709c013d3
Pawel Lipski <pawel.p.lipski@gmail.com>
2020-06-03 21:42:54 +0200

    Enhance UI tests + migrate to Jetbrains-provided Robot Remote/Server - 4th round of fixes

--- uiTests/src/test/java/com/virtuslab/gitmachete/uitest/UiTestSuite.java
+++ uiTests/src/test/java/com/virtuslab/gitmachete/uitest/UiTestSuite.java
@@ -6,30 +6,53 @@ import java.nio.file.Paths;
 import com.intellij.remoterobot.RemoteRobot;
 import lombok.SneakyThrows;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.virtuslab.gitmachete.testcommon.BaseGitRepositoryBackedTestSuite;
 
 public class UiTestSuite extends BaseGitRepositoryBackedTestSuite {
 
+  private final static String script = loadScript();
+
+  private RemoteRobot remoteRobot;
+
+  @SneakyThrows
+  private static String loadScript() {
+    return new String(
+        Files.readAllBytes(
+            Paths.get(
+                UiTestSuite.class.getResource("/scenarios-and-utils.rhino.js").toURI())));
+  }
+
+  @Before
+  public void connectToIde() {
+    remoteRobot = new RemoteRobot("http://127.0.0.1:8080");
+  }
+
   @Test
   @SneakyThrows
   public void openTabAndCountRows() {
     init(SETUP_WITH_SINGLE_REMOTE);
 
-    var script = new String(
-        Files.readAllBytes(
-            Paths.get(
-                getClass().getResource("/open-tab-and-count-rows.rhino.js").toURI())));
+    runJs("configureIde()");
+    runJs("closeOpenedProjects()");
+    runJs("openProject('${repositoryMainDir}')");
 
-    RemoteRobot remoteRobot = new RemoteRobot("http://127.0.0.1:8080");
-    int graphTableRowCount = remoteRobot.callJs(script + "openTabAndReturnRowCount('" + repositoryMainDir + "')",
-        /* runInEdt */ false);
+    int graphTableRowCount = callJs("openTabAndReturnRowCount(soleOpenedProject())");
 
     // There should be exactly 6 rows in the graph table, since there are 6 branches in machete file,
     // as set up via `init(SETUP_WITH_SINGLE_REMOTE)`.
     Assert.assertEquals(6, graphTableRowCount);
 
-    remoteRobot.runJs(script + "closeIde();", /* runInEdt */ false);
+    runJs("closeIde()");
+  }
+
+  private void runJs(String statement) {
+    remoteRobot.runJs(script + statement, /* runInEdt */ false);
+  }
+
+  private <T> T callJs(String expression) {
+    return remoteRobot.callJs(script + expression, /* runInEdt */ false);
   }
 }
