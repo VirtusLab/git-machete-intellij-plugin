d146fcb514ae8057c9366cbe758cb709a7dd1cc4
Mikołaj Kondratek <mkondratek@virtuslab.com>
2020-06-10 21:15:49 +0200

    [frontend] Track changes to machete files - gradlelike (closes #81)

---
+++ frontendExternalSystem/src/main/java/com/virtuslab/gitmachete/frontend/externalsystem/settings/MacheteExecutionSettings.java
@@ -0,0 +1,5 @@
+package com.virtuslab.gitmachete.frontend.externalsystem.settings;
+
+import com.intellij.openapi.externalSystem.model.settings.ExternalSystemExecutionSettings;
+
+public class MacheteExecutionSettings extends ExternalSystemExecutionSettings {}
