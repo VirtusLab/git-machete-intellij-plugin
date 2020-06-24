6a306026929eb3cae4308c7655bf854dc5854e1c
Michał Piotrowski <mpiotrowski@virtuslab.com>
2020-06-24 17:15:40 +0200

    Try to fix plugin compatibility problem when code annotations are displayed

--- frontendFile/src/main/java/com/virtuslab/gitmachete/frontend/file/highlighting/MacheteAnnotator.java
+++ frontendFile/src/main/java/com/virtuslab/gitmachete/frontend/file/highlighting/MacheteAnnotator.java
@@ -1,8 +1,11 @@
 package com.virtuslab.gitmachete.frontend.file.highlighting;
 
+import com.intellij.lang.annotation.Annotation;
 import com.intellij.lang.annotation.AnnotationHolder;
 import com.intellij.lang.annotation.Annotator;
 import com.intellij.lang.annotation.HighlightSeverity;
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.util.BuildNumber;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
 
@@ -33,8 +36,18 @@ public class MacheteAnnotator implements Annotator {
     var processedBranchName = branch.getText();
 
     if (!branchNames.contains(processedBranchName)) {
-      holder.newAnnotation(HighlightSeverity.ERROR, "Can't find '${processedBranchName}' branch in git repository")
-          .range(branch).create();
+      var buildNumberSinceAnnotationBuilderIsAvailable = BuildNumber.fromString("201.3803.32");
+      assert buildNumberSinceAnnotationBuilderIsAvailable != null : "Error while parsing build number";
+
+      // Remove this if when we drop support for every IntelliJ version bellow 201.3803.32
+      if (ApplicationInfo.getInstance().getBuild().compareTo(buildNumberSinceAnnotationBuilderIsAvailable) >= 0) {
+        holder.newAnnotation(HighlightSeverity.ERROR, "Can't find '${processedBranchName}' branch in git repository")
+            .range(branch).create();
+      } else {
+        Annotation errorAnnotation = holder.createErrorAnnotation(branch,
+            "Can't find '${processedBranchName}' branch in git repository");
+        errorAnnotation.setTextAttributes(MacheteSyntaxHighlighter.BAD_CHARACTER);
+      }
     }
   }
 }
