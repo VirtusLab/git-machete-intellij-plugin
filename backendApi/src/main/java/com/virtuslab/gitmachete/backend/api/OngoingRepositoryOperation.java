ba277eca62e16c84abb61335ed3a7cddfb006c53
Michał Piotrowski <mpiotrowski@virtuslab.com>
2020-07-16 21:51:20 +0200

    Change GitCoreRepositoryState and OngoingRepositoryOperation enums order to nearly lexical order

--- backendApi/src/main/java/com/virtuslab/gitmachete/backend/api/OngoingRepositoryOperation.java
+++ backendApi/src/main/java/com/virtuslab/gitmachete/backend/api/OngoingRepositoryOperation.java
@@ -1,5 +1,5 @@
 package com.virtuslab.gitmachete.backend.api;
 
 public enum OngoingRepositoryOperation {
-  NO_OPERATION, CHERRY_PICKING, MERGING, REBASING, REVERTING, APPLYING, BISECTING
+  NO_OPERATION, APPLYING, BISECTING, CHERRY_PICKING, MERGING, REBASING, REVERTING
 }
