c87ba00183b45de80f84125b39ddd0de81db890a
Mikołaj Kondratek <mkondratek@virtuslab.com>
2020-07-16 11:52:53 +0200

    Add kotlin formatter

--- frontendActions/src/main/java/com/virtuslab/gitmachete/frontend/actions/dialogs/SlideInDialog.kt
+++ frontendActions/src/main/java/com/virtuslab/gitmachete/frontend/actions/dialogs/SlideInDialog.kt
@@ -12,49 +12,57 @@ import kotlin.apply
 import kotlin.text.isEmpty
 import kotlin.text.trim
 
-class SlideInDialog constructor(project: Project,
-                                private val parentName: String)
-  : DialogWrapper(project, true) {
+class SlideInDialog constructor(project: Project, private val parentName: String) :
+    DialogWrapper(project, true) {
 
   // this field is only ever meant to be written on UI thread
   private var branchName = ""
 
   init {
     title = message("action.GitMachete.BaseSlideInBranchBelowAction.dialog.slide-in.title")
-    setOKButtonText(message("action.GitMachete.BaseSlideInBranchBelowAction.dialog.slide-in.ok-button"))
+    setOKButtonText(
+        message("action.GitMachete.BaseSlideInBranchBelowAction.dialog.slide-in.ok-button"))
     setOKButtonMnemonic('I'.toInt())
     init()
   }
 
   fun showAndGetBranchName() = if (showAndGet()) branchName.trim() else null
 
-  override fun createCenterPanel() = panel {
-    row(message("action.GitMachete.BaseSlideInBranchBelowAction.dialog.slide-in.label.parent")) {
-      label(parentName, bold = true)
-    }
-    row {
-      label(message("action.GitMachete.BaseSlideInBranchBelowAction.dialog.slide-in.label"))
-    }
-    row {
-      textField(::branchName, { branchName = it }).focused().withValidationOnApply(
-          validateBranchName()).apply { startTrackingValidationIfNeeded() }
-    }
-  }
+  override fun createCenterPanel() =
+      panel {
+        row(
+            message(
+                "action.GitMachete.BaseSlideInBranchBelowAction.dialog.slide-in.label.parent")) {
+          label(parentName, bold = true)
+        }
+        row {
+          label(message("action.GitMachete.BaseSlideInBranchBelowAction.dialog.slide-in.label"))
+        }
+        row {
+          textField(::branchName, { branchName = it })
+              .focused()
+              .withValidationOnApply(validateBranchName())
+              .apply { startTrackingValidationIfNeeded() }
+        }
+      }
 
-  private fun validateBranchName(): ValidationInfoBuilder.(javax.swing.JTextField) -> ValidationInfo? = {
-    val errorInfo = git4idea.validators.checkRefName(it.text)
-    if (errorInfo != null) error(errorInfo.message)
-    else null
-  }
+  private fun validateBranchName():
+      ValidationInfoBuilder.(javax.swing.JTextField) -> ValidationInfo? =
+      {
+        val errorInfo = git4idea.validators.checkRefName(it.text)
+        if (errorInfo != null) error(errorInfo.message) else null
+      }
 
   private fun CellBuilder<javax.swing.JTextField>.startTrackingValidationIfNeeded() {
     if (branchName.isEmpty()) {
-      component.document.addDocumentListener(object : DocumentAdapter() {
-        override fun textChanged(e: javax.swing.event.DocumentEvent) {
-          startTrackingValidation()
-          component.document.removeDocumentListener(this)
-        }
-      })
+      component.document
+          .addDocumentListener(
+              object : DocumentAdapter() {
+                override fun textChanged(e: javax.swing.event.DocumentEvent) {
+                  startTrackingValidation()
+                  component.document.removeDocumentListener(this)
+                }
+              })
     } else {
       startTrackingValidation()
     }
