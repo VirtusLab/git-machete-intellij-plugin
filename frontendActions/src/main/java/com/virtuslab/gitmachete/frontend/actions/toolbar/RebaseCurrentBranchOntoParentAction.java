1dd94ebc74593c11f2328eb05cae3a104df2e914
Mikołaj Kondratek <mkondratek@virtuslab.com>
2020-08-07 17:52:55 +0200

    [frontendActions][rebase] Hide rebase current (toolbar action) for roots & Reorganize toolbar actions

--- frontendActions/src/main/java/com/virtuslab/gitmachete/frontend/actions/toolbar/RebaseCurrentBranchOntoParentAction.java
+++ frontendActions/src/main/java/com/virtuslab/gitmachete/frontend/actions/toolbar/RebaseCurrentBranchOntoParentAction.java
@@ -2,6 +2,7 @@ package com.virtuslab.gitmachete.frontend.actions.toolbar;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import io.vavr.control.Option;
+import org.checkerframework.checker.guieffect.qual.UIEffect;
 
 import com.virtuslab.gitmachete.frontend.actions.base.BaseRebaseBranchOntoParentAction;
 
@@ -9,5 +10,20 @@ public class RebaseCurrentBranchOntoParentAction extends BaseRebaseBranchOntoPar
   @Override
   public Option<String> getNameOfBranchUnderAction(AnActionEvent anActionEvent) {
     return getCurrentBranchNameIfManaged(anActionEvent);
+  }
+
+  @Override
+  @UIEffect
+  public void onUpdate(AnActionEvent anActionEvent) {
+    var isEnabledAndVisible = getNameOfBranchUnderAction(anActionEvent)
+        .flatMap(bn -> getGitMacheteBranchByName(anActionEvent, bn))
+        .map(b -> b.isNonRootBranch())
+        .getOrElse(false);
+
+    anActionEvent.getPresentation().setEnabledAndVisible(isEnabledAndVisible);
+
+    if (isEnabledAndVisible) {
+      super.onUpdate(anActionEvent);
+    }
   }
 }
