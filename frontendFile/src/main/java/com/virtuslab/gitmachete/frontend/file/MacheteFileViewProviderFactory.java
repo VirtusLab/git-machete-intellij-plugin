7a96a79e0dc85e6f5b959d196c88d33690d46ded
Michał Piotrowski <mpiotrowski@virtuslab.com>
2020-12-04 12:27:45 +0100

    [frontendFile] Refresh machete file annotation when repo changed

--- frontendFile/src/main/java/com/virtuslab/gitmachete/frontend/file/MacheteFileViewProviderFactory.java
+++ frontendFile/src/main/java/com/virtuslab/gitmachete/frontend/file/MacheteFileViewProviderFactory.java
@@ -1,11 +1,19 @@
 package com.virtuslab.gitmachete.frontend.file;
 
+import static com.intellij.openapi.application.ModalityState.NON_MODAL;
+
+import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer;
 import com.intellij.lang.Language;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.FileViewProvider;
 import com.intellij.psi.FileViewProviderFactory;
 import com.intellij.psi.PsiManager;
 import com.intellij.psi.SingleRootFileViewProvider;
+import com.intellij.ui.GuiUtils;
+import com.intellij.util.messages.Topic;
+import git4idea.repo.GitRepository;
+import git4idea.repo.GitRepositoryChangeListener;
 
 public class MacheteFileViewProviderFactory implements FileViewProviderFactory {
   @Override
@@ -17,17 +25,27 @@ public class MacheteFileViewProviderFactory implements FileViewProviderFactory {
   }
 }
 
-class MacheteFileViewProvider extends SingleRootFileViewProvider {
+final class MacheteFileViewProvider extends SingleRootFileViewProvider {
 
   protected MacheteFileViewProvider(PsiManager manager,
       VirtualFile virtualFile,
       boolean eventSystemEnabled,
       Language language) {
     super(manager, virtualFile, eventSystemEnabled, language);
+    subscribeToGitRepositoryChanges(manager.getProject(), language);
   }
 
   @Override
   protected boolean shouldCreatePsi() {
     return true;
+  }
+
+  private void subscribeToGitRepositoryChanges(Project project, Language language) {
+    Topic<GitRepositoryChangeListener> topic = GitRepository.GIT_REPO_CHANGE;
+    GitRepositoryChangeListener listener = repository -> GuiUtils.invokeLaterIfNeeded(() -> {
+      DaemonCodeAnalyzer daemonCodeAnalyzer = DaemonCodeAnalyzer.getInstance(project);
+      daemonCodeAnalyzer.restart(getCachedPsi(language));
+    }, NON_MODAL);
+    project.getMessageBus().connect().subscribe(topic, listener);
   }
 }
