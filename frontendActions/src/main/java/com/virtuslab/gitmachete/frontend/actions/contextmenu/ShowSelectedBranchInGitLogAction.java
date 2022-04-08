aa3467c2063597847fb1a3521d9711931ecbac61
mkondratek <mik.kondratek@gmail.com>
2022-04-08 16:36:57 +0200

    Add context menu action to show branch in Git log - mention changes in change notes - bump version

---
+++ frontendActions/src/main/java/com/virtuslab/gitmachete/frontend/actions/contextmenu/ShowSelectedBranchInGitLogAction.java
@@ -0,0 +1,102 @@
+package com.virtuslab.gitmachete.frontend.actions.contextmenu;
+
+import static com.virtuslab.gitmachete.frontend.resourcebundles.GitMacheteBundle.format;
+import static com.virtuslab.gitmachete.frontend.resourcebundles.GitMacheteBundle.getString;
+
+import java.util.Objects;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.ExecutionException;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.progress.ProgressIndicator;
+import com.intellij.openapi.progress.Task;
+import com.intellij.openapi.project.Project;
+import com.intellij.vcs.log.Hash;
+import com.intellij.vcs.log.impl.VcsLogContentUtil;
+import com.intellij.vcs.log.ui.VcsLogUiEx;
+import git4idea.branch.GitBranchesCollection;
+import lombok.CustomLog;
+import lombok.val;
+import org.checkerframework.checker.guieffect.qual.UIEffect;
+
+import com.virtuslab.gitmachete.frontend.actions.base.BaseGitMacheteRepositoryReadyAction;
+import com.virtuslab.gitmachete.frontend.actions.expectedkeys.IExpectsKeySelectedBranchName;
+import com.virtuslab.logger.IEnhancedLambdaLogger;
+
+@CustomLog
+public class ShowSelectedBranchInGitLogAction extends BaseGitMacheteRepositoryReadyAction
+    implements
+      IExpectsKeySelectedBranchName {
+
+  @Override
+  public IEnhancedLambdaLogger log() {
+    return LOG;
+  }
+
+  @Override
+  @UIEffect
+  protected void onUpdate(AnActionEvent anActionEvent) {
+    super.onUpdate(anActionEvent);
+
+    val presentation = anActionEvent.getPresentation();
+    if (!presentation.isEnabledAndVisible()) {
+      return;
+    }
+
+    val selectedBranchName = getSelectedBranchName(anActionEvent);
+    // It's very unlikely that selectedBranchName is empty at this point since it's assigned directly before invoking this
+    // action in GitMacheteGraphTable.GitMacheteGraphTableMouseAdapter.mouseClicked; still, it's better to be safe.
+    if (selectedBranchName.isEmpty()) {
+      presentation.setEnabled(false);
+      presentation.setDescription(getString("action.GitMachete.ShowSelectedBranchInGitLogAction.undefined.branch-name"));
+      return;
+    }
+
+    presentation.setDescription(
+        format(getString("action.GitMachete.ShowSelectedBranchInGitLogAction.description.precise"), selectedBranchName.get()));
+  }
+
+  @Override
+  @UIEffect
+  public void actionPerformed(AnActionEvent anActionEvent) {
+    val selectedBranchName = getSelectedBranchName(anActionEvent);
+    if (selectedBranchName.isEmpty()) {
+      return;
+    }
+
+    val project = getProject(anActionEvent);
+    val gitRepository = getSelectedGitRepository(anActionEvent);
+
+    if (gitRepository.isDefined()) {
+      log().debug(() -> "Queuing show '${selectedBranchName.get()}' branch in Git log background task");
+
+      GitBranchesCollection branches = gitRepository.get().getBranches();
+      @SuppressWarnings({"nullness:argument", "nullness:return"}) val maybeHash = selectedBranchName
+          .map(branches::findBranchByName).filter(Objects::nonNull).map(branches::getHash);
+      if (maybeHash.isEmpty()) {
+        log().error("Unable to find commit hash for branch '${selectedBranchName.get()}'");
+        return;
+      }
+
+      VcsLogContentUtil.runInMainLog(project, logUi -> jumpToRevisionUnderProgress(project, maybeHash.get(), logUi));
+    }
+  }
+
+  private void jumpToRevisionUnderProgress(Project project, Hash hash, VcsLogUiEx logUi) {
+    new Task.Backgroundable(project, getString("action.GitMachete.ShowSelectedBranchInGitLogAction.task-title")) {
+      @Override
+      public void run(ProgressIndicator indicator) {
+        try {
+          logUi.getVcsLog().jumpToReference(hash.asString()).get();
+        } catch (CancellationException | InterruptedException ignored) {} catch (ExecutionException e) {
+          String msg = "Error while showing branch in Git log";
+          if (e.getMessage() != null) {
+            msg = e.getMessage();
+          }
+          log().error(msg);
+        }
+      }
+    }.queue();
+  }
+
+}
