1bf58dbef9c66fb9493ff3aecbe015911c563c3f
mkondratek <mik.kondratek@gmail.com>
2022-04-26 12:54:53 +0200

    Improve messaging and formatting on the reset info dialog

--- frontendActions/src/main/kotlin/com/virtuslab/gitmachete/frontend/actions/dialogs/ResetBranchToRemoteInfoDialog.kt
+++ frontendActions/src/main/kotlin/com/virtuslab/gitmachete/frontend/actions/dialogs/ResetBranchToRemoteInfoDialog.kt
@@ -4,6 +4,7 @@ import com.intellij.ide.util.PropertiesComponent
 import com.intellij.openapi.ui.DialogWrapper
 import com.intellij.openapi.ui.Messages
 import com.virtuslab.gitmachete.frontend.actions.base.BaseResetBranchToRemoteAction.RESET_INFO_SHOWN
+import com.virtuslab.gitmachete.frontend.resourcebundles.GitMacheteBundle
 
 @Suppress("DEPRECATION")
 class ResetBranchToRemoteInfoDialog : DialogWrapper.DoNotAskOption.Adapter() {
@@ -11,5 +12,10 @@ class ResetBranchToRemoteInfoDialog : DialogWrapper.DoNotAskOption.Adapter() {
     if (exitCode == Messages.OK && isSelected) {
       PropertiesComponent.getInstance().setValue(RESET_INFO_SHOWN, true)
     }
+  }
+
+  override fun getDoNotShowMessage(): String {
+    return GitMacheteBundle.getString(
+        "action.GitMachete.BaseResetBranchToRemoteAction.info-dialog.do-not-show-again")
   }
 }
