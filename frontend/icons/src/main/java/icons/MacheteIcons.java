9052ed49bfa84d12377f36ea91cd441136eee2ed
mkondratek <mik.kondratek@gmail.com>
2022-05-06 12:16:46 +0200

    Icons update

--- frontend/icons/src/main/java/icons/MacheteIcons.java
+++ frontend/icons/src/main/java/icons/MacheteIcons.java
@@ -15,7 +15,7 @@ public final class MacheteIcons {
 
   public static final Icon FAST_FORWARD = loadIcon("/applyNotConflictsLeft.svg");
 
-  public static final Icon FETCH = loadIcon("/download.svg");
+  public static final Icon FETCH = loadIcon("/fetch.svg");
 
   public static final Icon HELP = loadIcon("/help.svg");
 
@@ -25,11 +25,11 @@ public final class MacheteIcons {
 
   public static final Icon OVERRIDE_FORK_POINT = loadIcon("/overrideForkPoint.svg");
 
-  public static final Icon PULL = loadIcon("/incomingChangesOn.svg");
+  public static final Icon PULL = loadIcon("/pull.svg");
 
-  public static final Icon PUSH = loadIcon("/outgoingChangesOn.svg");
+  public static final Icon PUSH = loadIcon("/push.svg");
 
-  public static final Icon REBASE = loadIcon("/menu-cut.svg");
+  public static final Icon REBASE = loadIcon("/menuCut.svg");
 
   public static final Icon REFRESH_STATUS = loadIcon("/refresh.svg");
 
@@ -39,9 +39,9 @@ public final class MacheteIcons {
 
   public static final Icon TOGGLE_LISTING_COMMITS = loadIcon("/showHiddens.svg");
 
-  public static final Icon SLIDE_IN = loadIcon("/branch.svg");
+  public static final Icon SLIDE_IN = loadIcon("/slideIn.svg");
 
-  public static final Icon SLIDE_OUT = loadIcon("/gc.svg");
+  public static final Icon SLIDE_OUT = loadIcon("/slideOut.svg");
 
   private static Icon loadIcon(String path) {
     return IconLoader.getIcon(path, MacheteIcons.class);
