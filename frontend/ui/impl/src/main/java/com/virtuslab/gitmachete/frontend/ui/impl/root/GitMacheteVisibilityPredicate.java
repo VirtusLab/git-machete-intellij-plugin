f9e52650ad1eeba01483409ff279d90ca5afcac8
mkondratek <mik.kondratek@gmail.com>
2022-05-12 16:35:26 +0200

    Use same visibility predicate as VCS log for Git Machete tab (closes #706)

--- frontend/ui/impl/src/main/java/com/virtuslab/gitmachete/frontend/ui/impl/root/GitMacheteVisibilityPredicate.java
+++ frontend/ui/impl/src/main/java/com/virtuslab/gitmachete/frontend/ui/impl/root/GitMacheteVisibilityPredicate.java
@@ -1,9 +1,8 @@
 package com.virtuslab.gitmachete.frontend.ui.impl.root;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.vcs.ProjectLevelVcsManager;
 import com.intellij.util.NotNullFunction;
-import git4idea.GitVcs;
+import com.intellij.vcs.log.impl.VcsProjectLog;
 import lombok.CustomLog;
 
 @CustomLog
@@ -11,10 +10,7 @@ public class GitMacheteVisibilityPredicate implements NotNullFunction<Project, B
 
   @Override
   public Boolean fun(Project project) {
-    // `com.intellij.openapi.vcs.ProjectLevelVcsManager#checkVcsIsActive(String)` returns `true` when the specified
-    // VCS is used by at least one module in the project. Therefore it is guaranteed that Git Machete plugin tab
-    // is created only when a git repository exists.
-    boolean predicateResult = ProjectLevelVcsManager.getInstance(project).checkVcsIsActive(GitVcs.NAME);
+    boolean predicateResult = !VcsProjectLog.getLogProviders(project).isEmpty();
     LOG.debug(() -> "Visibility predicate returned ${predicateResult}");
     return predicateResult;
   }
