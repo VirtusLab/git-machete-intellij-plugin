7e1a5798a081a51151a223dcaeb46d10799ce399
mkondratek <mik.kondratek@gmail.com>
2022-06-23 16:58:32 +0200

    Drop support for 2021.1

--- frontend/ui/impl/src/main/java/com/virtuslab/gitmachete/frontend/ui/impl/root/GitMacheteVisibilityPredicate.java
+++ frontend/ui/impl/src/main/java/com/virtuslab/gitmachete/frontend/ui/impl/root/GitMacheteVisibilityPredicate.java
@@ -1,15 +1,15 @@
 package com.virtuslab.gitmachete.frontend.ui.impl.root;
 
 import com.intellij.openapi.project.Project;
-import com.intellij.util.NotNullFunction;
 import com.intellij.vcs.log.impl.VcsProjectLog;
+import java.util.function.Predicate;
 import lombok.CustomLog;
 
 @CustomLog
-public class GitMacheteVisibilityPredicate implements NotNullFunction<Project, Boolean> {
+public class GitMacheteVisibilityPredicate implements Predicate<Project> {
 
   @Override
-  public Boolean fun(Project project) {
+  public boolean test(Project project) {
     boolean predicateResult = !VcsProjectLog.getLogProviders(project).isEmpty();
     LOG.debug(() -> "Visibility predicate returned ${predicateResult}");
     return predicateResult;
