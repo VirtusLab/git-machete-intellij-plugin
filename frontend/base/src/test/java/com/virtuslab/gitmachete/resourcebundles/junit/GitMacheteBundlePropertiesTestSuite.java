e658fd6b4e54e9de06ce90a7150ed5e2857fb1f0
Maciej Gajek <mgajek@virtuslab.com>
2022-07-06 12:36:55 +0200

    Moved loading properties to static method + refactor of properties iteration, regex and messages

--- frontend/base/src/test/java/com/virtuslab/gitmachete/resourcebundles/junit/GitMacheteBundlePropertiesTestSuite.java
+++ frontend/base/src/test/java/com/virtuslab/gitmachete/resourcebundles/junit/GitMacheteBundlePropertiesTestSuite.java
@@ -10,16 +10,22 @@ import org.junit.Test;
 
 public class GitMacheteBundlePropertiesTestSuite {
 
-  private final String macheteBundleProperties = "GitMacheteBundle.properties";
+  private static final String macheteBundleProperties = "GitMacheteBundle.properties";
+
+  private static Properties loadProperties() throws IOException {
+    Properties properties = new Properties();
+    ClassLoader loader = Thread.currentThread().getContextClassLoader();
+    properties.load(loader.getResourceAsStream(macheteBundleProperties));
+
+    return properties;
+  }
 
   @Test
   public void htmlProperties_should_have_correct_syntax() throws IOException {
 
-    Properties property = new Properties();
-    ClassLoader loader = Thread.currentThread().getContextClassLoader();
-    property.load(loader.getResourceAsStream(macheteBundleProperties));
+    Properties properties = loadProperties();
 
-    property.entrySet().stream()
+    properties.entrySet().stream()
         .filter(prop -> ((String) prop.getValue()).contains("<"))
         .forEach(t -> {
           String key = (String) t.getKey();
@@ -33,40 +39,33 @@ public class GitMacheteBundlePropertiesTestSuite {
   @Test
   public void properties_should_have_no_double_quote_wrapping() throws IOException {
 
-    Properties property = new Properties();
-    ClassLoader loader = Thread.currentThread().getContextClassLoader();
-    property.load(loader.getResourceAsStream(macheteBundleProperties));
+    Properties properties = loadProperties();
 
-    property.entrySet().stream()
-        .filter(prop -> ((String) prop.getValue()).contains("\""))
-        .forEach(t -> {
-          String key = (String) t.getKey();
-          String value = (String) t.getValue();
-          assertFalse(
-              "Key '${key}' has value wrapped in double quotes (\"). Remove unnecessary wrapping",
-              value.endsWith("\"") && value.startsWith("\""));
-        });
+    properties.forEach((keyObj, valueObj) -> {
+      String key = (String) keyObj;
+      String value = (String) valueObj;
+      assertFalse(
+          "Key '${key}' has value wrapped in double quotes (\"). Remove unnecessary wrapping",
+          value.endsWith("\"") && value.startsWith("\""));
+    });
   }
 
   @Test
   public void properties_should_contain_only_valid_single_quotes() throws IOException {
 
-    Properties property = new Properties();
-    ClassLoader loader = Thread.currentThread().getContextClassLoader();
-    property.load(loader.getResourceAsStream(macheteBundleProperties));
+    Properties properties = loadProperties();
 
-    property.entrySet().stream()
-        .forEach(t -> {
-          String key = (String) t.getKey();
-          String value = (String) t.getValue();
-          assertFalse(
-              "Key '${key}' has a format element ({number}), but contains single quote (')." +
-                  "Use double single quote instead ('')",
-              value.matches(".*\\{\\d+}.*") && value.matches(".*((^'[^'])|([^']'[^'])|([^']'$)).*"));
-          assertFalse(
-              "Key '${key}' has NO format element ({number}), but contains double single quote ('')." +
-                  "Use single quote instead (')",
-              !value.matches(".*\\{\\d+}.*") && value.matches(".*''.*"));
-        });
+    properties.forEach((keyObj, valueObj) -> {
+      String key = (String) keyObj;
+      String value = (String) valueObj;
+      assertFalse(
+          "Key '${key}' has a format element ({number}), but contains a single apostrophe (')." +
+              "Use a double apostrophe ('') instead",
+          value.matches(".*\\{\\d+}.*") && value.matches(".*(^'[^']|[^']'[^']|[^']'$).*"));
+      assertFalse(
+          "Key '${key}' has NO format element ({number}), but contains a double apostrophe ('')." +
+              "Use a single apostrophe (') instead",
+          !value.matches(".*\\{\\d+}.*") && value.matches(".*''.*"));
+    });
   }
 }
