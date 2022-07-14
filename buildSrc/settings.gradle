9f8d2c85df706ed63a83c2133040dda20a29f40c
Maciej Gajek <mgajek@virtuslab.com>
2022-07-14 14:20:22 +0200

    Added spotless support for buildSrc/, encapsulated file ops

--- buildSrc/settings.gradle
+++ buildSrc/settings.gradle
@@ -1,9 +1,9 @@
 enableFeaturePreview("VERSION_CATALOGS")
 
 dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") {
-            from(files("../gradle/libs.versions.toml"))
-        }
+  versionCatalogs {
+    create("libs") {
+      from(files("../gradle/libs.versions.toml"))
     }
+  }
 }
--- buildSrc/src/main/groovy/com.virtuslab.gitmachete.buildsrc/UpdateEapBuildNumber.groovy
+++ buildSrc/src/main/groovy/com.virtuslab.gitmachete.buildsrc/UpdateEapBuildNumber.groovy
@@ -15,45 +15,44 @@ import java.util.regex.Pattern
 
 class UpdateEapBuildNumber extends DefaultTask {
 
-    final String intellijSnapshotsUrl = "https://www.jetbrains.com/intellij-repository/snapshots/"
+  final String intellijSnapshotsUrl = "https://www.jetbrains.com/intellij-repository/snapshots/"
 
-    @Input
-    @Optional
-    @Option(option = 'exit-code', description = 'Return the exit code 0 when the new eap build number is found and 1 otherwise')
-    Boolean exitCode
+  @Input
+  @Optional
+  @Option(option = 'exit-code', description = 'Return the exit code 0 when the new eap build number is found and 1 otherwise')
+  Boolean exitCode
 
-    private String checkForEapWithBuildNumberHigherThan(String latestEapBuildNumber) {
-        Document htmlContent = Jsoup.connect(intellijSnapshotsUrl).get()
-        Elements links = htmlContent.select('a[href^=' + intellijSnapshotsUrl + 'com/jetbrains/intellij/idea/ideaIC/][href$="-EAP-SNAPSHOT.pom"]')
-        for (Element link : links) {
-            String attr = link.attr("href")
-            Pattern pattern = Pattern.compile("(?<=ideaIC-)\\d+\\.\\d+\\.\\d+(?=-EAP-SNAPSHOT.pom)")
-            Matcher matcher = pattern.matcher(attr)
-            if (matcher.find()) {
-                String foundBuildNumber = matcher.group()
-                if (VersionNumber.parse(foundBuildNumber) > VersionNumber.parse(latestEapBuildNumber)) {
-                    return foundBuildNumber
-                }
-            }
+  private String checkForEapWithBuildNumberHigherThan(String latestEapBuildNumber) {
+    Document htmlContent = Jsoup.connect(intellijSnapshotsUrl).get()
+    Elements links = htmlContent.select('a[href^=' + intellijSnapshotsUrl + 'com/jetbrains/intellij/idea/ideaIC/][href$="-EAP-SNAPSHOT.pom"]')
+    for (Element link : links) {
+      String attr = link.attr("href")
+      Pattern pattern = Pattern.compile("(?<=ideaIC-)\\d+\\.\\d+\\.\\d+(?=-EAP-SNAPSHOT.pom)")
+      Matcher matcher = pattern.matcher(attr)
+      if (matcher.find()) {
+        String foundBuildNumber = matcher.group()
+        if (VersionNumber.parse(foundBuildNumber) > VersionNumber.parse(latestEapBuildNumber)) {
+          return foundBuildNumber
         }
-        return null
+      }
     }
+    return null
+  }
 
-    @TaskAction
-    void execute() {
-        Properties properties = IntellijVersionHelper.getProperties()
-        File propFile = IntellijVersionHelper.getFile()
-        String latestEapBuildNumber = project.intellijVersions.eapOfLatestSupportedMajor
-        String buildNumberThreshold = latestEapBuildNumber ? latestEapBuildNumber.replace('-EAP-SNAPSHOT', '') :
-                project.intellijVersionToBuildNumber(project.intellijVersions.latestStable)+'.999999.999999'
-        String newerEapBuildNumber = checkForEapWithBuildNumberHigherThan(buildNumberThreshold)
-        if (newerEapBuildNumber) {
-            project.logger.lifecycle("EapOfLatestSupportedMajor is updated to " + newerEapBuildNumber + " build number")
-            properties.setProperty("eapOfLatestSupportedMajor", newerEapBuildNumber+"-EAP-SNAPSHOT")
-            properties.store(propFile.newWriter(), /*comments*/ null)
-        }
-        if (exitCode && !newerEapBuildNumber) {
-            throw new Exception("New eap build number not found")
-        }
+  @TaskAction
+  void execute() {
+    Properties properties = IntellijVersionHelper.getProperties()
+    String latestEapBuildNumber = project.intellijVersions.eapOfLatestSupportedMajor
+    String buildNumberThreshold = latestEapBuildNumber ? latestEapBuildNumber.replace('-EAP-SNAPSHOT', '') :
+        project.intellijVersionToBuildNumber(project.intellijVersions.latestStable)+'.999999.999999'
+    String newerEapBuildNumber = checkForEapWithBuildNumberHigherThan(buildNumberThreshold)
+    if (newerEapBuildNumber) {
+      project.logger.lifecycle("EapOfLatestSupportedMajor is updated to " + newerEapBuildNumber + " build number")
+      properties.setProperty("eapOfLatestSupportedMajor", newerEapBuildNumber+"-EAP-SNAPSHOT")
+      IntellijVersionHelper.store(properties)
     }
+    if (exitCode && !newerEapBuildNumber) {
+      throw new Exception("New eap build number not found")
+    }
+  }
 }
