7562e10be159d7ffd31e01c11e910f8189bbde9b
Maciej Gajek <mgajek@virtuslab.com>
2022-07-18 11:15:21 +0200

    Fixed IntelliJ highlighting error due to incompatible types

--- frontend/ui/impl/src/main/java/com/virtuslab/gitmachete/frontend/ui/impl/root/GitMacheteVisibilityPredicate.java
+++ frontend/ui/impl/src/main/java/com/virtuslab/gitmachete/frontend/ui/impl/root/GitMacheteVisibilityPredicate.java
@@ -1,17 +1,24 @@
 package com.virtuslab.gitmachete.frontend.ui.impl.root;
 
+import java.util.function.Predicate;
+
 import com.intellij.openapi.project.Project;
 import com.intellij.util.NotNullFunction;
 import com.intellij.vcs.log.impl.VcsProjectLog;
 import lombok.CustomLog;
 
 @CustomLog
-public class GitMacheteVisibilityPredicate implements NotNullFunction<Project, Boolean> {
+public class GitMacheteVisibilityPredicate implements NotNullFunction<Project, Boolean>, Predicate<Project> {
 
   @Override
   public Boolean fun(Project project) {
     boolean predicateResult = !VcsProjectLog.getLogProviders(project).isEmpty();
     LOG.debug(() -> "Visibility predicate returned ${predicateResult}");
     return predicateResult;
+  }
+
+  @Override
+  public boolean test(Project project) {
+    return fun(project);
   }
 }
