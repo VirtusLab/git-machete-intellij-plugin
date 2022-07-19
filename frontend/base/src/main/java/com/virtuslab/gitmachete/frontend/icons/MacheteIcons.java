d53156c02ea5111cbf2f599ffc402b2307ab2ca3
Maciej Gajek <mgajek@virtuslab.com>
2022-07-19 15:20:48 +0200

    Simplifiy icons loading, prettify code

--- frontend/base/src/main/java/com/virtuslab/gitmachete/frontend/icons/MacheteIcons.java
+++ frontend/base/src/main/java/com/virtuslab/gitmachete/frontend/icons/MacheteIcons.java
@@ -11,40 +11,40 @@ import com.intellij.openapi.util.IconLoader;
 public final class MacheteIcons {
   private MacheteIcons() {}
 
-  public static final Icon DISCOVER = loadIcon("/icons/macheteLogoIcon.svg");
+  public static final Icon DISCOVER = loadIcon("macheteLogoIcon");
 
-  public static final Icon EDIT = loadIcon("/icons/edit.svg");
+  public static final Icon EDIT = loadIcon("edit");
 
-  public static final Icon FAST_FORWARD = loadIcon("/icons/fastForward.svg");
+  public static final Icon FAST_FORWARD = loadIcon("fastForward");
 
-  public static final Icon FETCH = loadIcon("/icons/fetch.svg");
+  public static final Icon FETCH = loadIcon("fetch");
 
-  public static final Icon HELP = loadIcon("/icons/help.svg");
+  public static final Icon HELP = loadIcon("help");
 
-  public static final Icon MACHETE_FILE = loadIcon("/icons/macheteLogoIcon.svg");
+  public static final Icon MACHETE_FILE = loadIcon("macheteLogoIcon");
 
-  public static final Icon MERGE_PARENT = loadIcon("/icons/merge.svg");
+  public static final Icon MERGE_PARENT = loadIcon("merge");
 
-  public static final Icon OVERRIDE_FORK_POINT = loadIcon("/icons/overrideForkPoint.svg");
+  public static final Icon OVERRIDE_FORK_POINT = loadIcon("overrideForkPoint");
 
-  public static final Icon PULL = loadIcon("/icons/pull.svg");
+  public static final Icon PULL = loadIcon("pull");
 
-  public static final Icon PUSH = loadIcon("/icons/push.svg");
+  public static final Icon PUSH = loadIcon("push");
 
-  public static final Icon REBASE = loadIcon("/icons/rebase.svg");
+  public static final Icon REBASE = loadIcon("rebase");
 
-  public static final Icon REFRESH_STATUS = loadIcon("/icons/refresh.svg");
+  public static final Icon REFRESH_STATUS = loadIcon("refresh");
 
-  public static final Icon RESET = loadIcon("/icons/reset.svg");
+  public static final Icon RESET = loadIcon("reset");
 
-  public static final Icon SLIDE_IN = loadIcon("/icons/slideIn.svg");
+  public static final Icon SLIDE_IN = loadIcon("slideIn");
 
-  public static final Icon SLIDE_OUT = loadIcon("/icons/slideOut.svg");
+  public static final Icon SLIDE_OUT = loadIcon("slideOut");
 
-  public static final Icon TOGGLE_LISTING_COMMITS = loadIcon("/icons/toggleListingCommits.svg");
+  public static final Icon TOGGLE_LISTING_COMMITS = loadIcon("toggleListingCommits");
 
-  private static Icon loadIcon(String path) {
-    return IconLoader.getIcon(path, MacheteIcons.class);
+  private static Icon loadIcon(String basename) {
+    return IconLoader.getIcon("/icons/${basename}.svg", MacheteIcons.class);
   }
 
 }
