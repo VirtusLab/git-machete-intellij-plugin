ffcf57ba2cc2581c86164c6d39e497ec571d652d
Maciej Gajek <mgajek@virtuslab.com>
2022-07-28 15:35:12 +0200

    Use Junit in tests

--- buildSrc/src/test/kotlin/BuildNumberComparisonTest.kt
+++ buildSrc/src/test/kotlin/BuildNumberComparisonTest.kt
@@ -1,13 +1,15 @@
 import com.virtuslab.gitmachete.buildsrc.UpdateEapBuildNumber.Companion.buildNumberIsNewerThan
 import org.junit.Test
+import org.junit.Assert.assertTrue
+import org.junit.Assert.assertFalse
 
 class BuildNumberComparisonTest {
   @Test
   fun shouldRecognizeEqualVersions() {
     val version1 = "123.1234.56789"
     val version2 = "0.0.0"
-    assert(!version1.buildNumberIsNewerThan(version1))
-    assert(!version2.buildNumberIsNewerThan(version2))
+    assertFalse(version1 buildNumberIsNewerThan version1)
+    assertFalse(version2 buildNumberIsNewerThan version2)
   }
 
   @Test
@@ -15,6 +17,6 @@ class BuildNumberComparisonTest {
     val olderVersion = "1111.11.111"
     val newerVersions = listOf("1112.11.111", "1111.12.111", "1111.11.112", "1111.11.111.0")
 
-    for (newerVersion in newerVersions) assert(newerVersion.buildNumberIsNewerThan(olderVersion))
+    for (newerVersion in newerVersions) assertTrue(newerVersion buildNumberIsNewerThan olderVersion)
   }
 }
