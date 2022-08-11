4cc3c79ad070b98fc57399a4b2d3918f5382d4be
Maciej Gajek <mgajek@virtuslab.com>
2022-08-11 19:10:49 +0200

    Unmock tested class' method

--- frontend/ui/impl/src/test/java/com/virtuslab/gitmachete/frontend/ui/impl/GitMacheteErrorReportSubmitterTest.java
+++ frontend/ui/impl/src/test/java/com/virtuslab/gitmachete/frontend/ui/impl/GitMacheteErrorReportSubmitterTest.java
@@ -6,14 +6,17 @@ import static org.mockito.Mockito.never;
 import java.io.PrintWriter;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
 
+import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent;
+import com.intellij.openapi.extensions.PluginId;
 import kr.pe.kwonnam.slf4jlambda.LambdaLogger;
 import lombok.SneakyThrows;
 import lombok.val;
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.SystemUtils;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,41 +27,44 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({GitMacheteErrorReportSubmitter.class})
+@PrepareForTest({GitMacheteErrorReportSubmitter.class, ApplicationInfo.class, PluginId.class, System.class, SystemUtils.class})
 public class GitMacheteErrorReportSubmitterTest {
 
   // Used spy(...) here to still keep the original behaviour of the class and be able to stub getTemplateVariables method
   // PowerMock API documentation Section 13 [https://javadoc.io/static/org.mockito/mockito-core/4.6.1/org/mockito/Mockito.html]
-  private static final GitMacheteErrorReportSubmitter gitMacheteErrorReportSubmitter = PowerMockito
+  private static final GitMacheteErrorReportSubmitter reportSubmitter = PowerMockito
       .spy(new GitMacheteErrorReportSubmitter());
   private static final LambdaLogger errorReportSubmitterLogger = PowerMockito.mock(LambdaLogger.class);
   private static String defaultErrorReport;
   private static String defaultErrorReportWithTemplate;
   private static String errorReportWithStackTraceAndTitle;
+  private static String systemLineSeparator = "\n";
 
   @BeforeClass
   @SneakyThrows
-  public static void setUp() {
+  public static void setUpStatic() {
     defaultErrorReport = IOUtils.resourceToString("/error_report_uri.txt", StandardCharsets.UTF_8).strip();
     defaultErrorReportWithTemplate = IOUtils.resourceToString("/error_report_uri_with_template.txt",
         StandardCharsets.UTF_8).strip();
     errorReportWithStackTraceAndTitle = IOUtils.resourceToString("/error_report_uri_with_stacktrace_title.txt",
         StandardCharsets.UTF_8).strip();
+  }
 
+  @Before
+  public void setUp() {
     Whitebox.setInternalState(GitMacheteErrorReportSubmitter.class, errorReportSubmitterLogger);
-  }
 
-  private GitMacheteErrorReportSubmitter getMockReportSubmitter() {
-    return getMockReportSubmitter(new HashMap<>());
-  }
+    PowerMockito.mockStatic(ApplicationInfo.class);
+    val applicationInfo = PowerMockito.mock(ApplicationInfo.class);
+    PowerMockito.when(applicationInfo.getFullApplicationName()).thenReturn("mocked IntelliJ idea");
+    PowerMockito.stub(PowerMockito.method(ApplicationInfo.class, "getInstance"))
+        .toReturn(applicationInfo);
 
-  private GitMacheteErrorReportSubmitter getMockReportSubmitter(HashMap<String, String> templateVariables) {
-    PowerMockito
-        .stub(PowerMockito.method(GitMacheteErrorReportSubmitter.class, "getTemplateVariables", IdeaLoggingEvent[].class,
-            String.class))
-        .toReturn(templateVariables);
-
-    return gitMacheteErrorReportSubmitter;
+    PowerMockito.mockStatic(PluginId.class);
+    PowerMockito.stub(PowerMockito.method(System.class, "lineSeparator"))
+        .toReturn(systemLineSeparator);
+    Whitebox.setInternalState(SystemUtils.class, "OS_NAME", "Mock OS X");
+    Whitebox.setInternalState(SystemUtils.class, "OS_VERSION", "Hehe");
   }
 
   private IdeaLoggingEvent[] getMockEvents(String throwableText) {
@@ -89,7 +95,6 @@ public class GitMacheteErrorReportSubmitterTest {
   @Test
   public void shouldConstructUriWithTemplate() throws Exception {
     val events = getMockEvents(/* throwableText */ "");
-    val reportSubmitter = getMockReportSubmitter();
     val reportBody = invokeGetReportBody(reportSubmitter, events);
 
     URI uriWithReport = Whitebox.invokeMethod(reportSubmitter, "constructNewGitHubIssueUri", events, reportBody);
@@ -99,7 +104,6 @@ public class GitMacheteErrorReportSubmitterTest {
   @Test
   public void shouldConstructUriWithNoTemplate() throws Exception {
     val events = getMockEvents(/* throwableText */ "");
-    val reportSubmitter = getMockReportSubmitter();
 
     URI uriNoReport = Whitebox.invokeMethod(reportSubmitter, "constructNewGitHubIssueUri", events, /* report Body */ null);
     Assert.assertEquals("URI should match default with NO report body", defaultErrorReport, uriNoReport.toString());
@@ -107,15 +111,8 @@ public class GitMacheteErrorReportSubmitterTest {
 
   @Test
   public void shouldConstructUriWithTitleAndStackTrace() throws Exception {
-    val sep = System.lineSeparator();
-    String throwableText = "Title - top of the stacktrace${sep}stacktrace${sep}stacktrace";
-
+    String throwableText = "Title - top of the stacktrace${systemLineSeparator}stacktrace${systemLineSeparator}stacktrace";
     val events = getMockEvents(throwableText);
-
-    HashMap<String, String> templateVariables = new HashMap<>();
-    templateVariables.put("stacktraces", "```${sep}${throwableText}${sep}```");
-
-    val reportSubmitter = getMockReportSubmitter(templateVariables);
     val reportBody = invokeGetReportBody(reportSubmitter, events);
 
     URI uri = Whitebox.invokeMethod(reportSubmitter, "constructNewGitHubIssueUri", events, reportBody);
