da2d8e98674bde6bca19cf2bd481ed50934c4689
Pawel Lipski <pawel.p.lipski@gmail.com>
2022-08-12 18:22:10 +0200

    Massage GitMacheteErrorReportSubmitter and its test a bit more

--- frontend/ui/impl/src/test/java/com/virtuslab/gitmachete/frontend/ui/impl/GitMacheteErrorReportSubmitterTest.java
+++ frontend/ui/impl/src/test/java/com/virtuslab/gitmachete/frontend/ui/impl/GitMacheteErrorReportSubmitterTest.java
@@ -1,15 +1,13 @@
 package com.virtuslab.gitmachete.frontend.ui.impl;
 
-import static org.mockito.ArgumentMatchers.*;
-import static org.mockito.Mockito.never;
-
-import java.io.PrintWriter;
 import java.net.URI;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
+import com.intellij.ide.plugins.IdeaPluginDescriptor;
+import com.intellij.ide.plugins.PluginManagerCore;
 import com.intellij.openapi.application.ApplicationInfo;
 import com.intellij.openapi.diagnostic.IdeaLoggingEvent;
-import com.intellij.openapi.extensions.PluginId;
 import kr.pe.kwonnam.slf4jlambda.LambdaLogger;
 import lombok.SneakyThrows;
 import lombok.val;
@@ -20,104 +18,80 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 import org.powermock.reflect.Whitebox;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({GitMacheteErrorReportSubmitter.class, ApplicationInfo.class, PluginId.class, System.class, SystemUtils.class})
+@PrepareForTest({ApplicationInfo.class, PluginManagerCore.class, SystemUtils.class})
 public class GitMacheteErrorReportSubmitterTest {
 
-  // Used spy(...) here to still keep the original behaviour of the class and be able to stub getTemplateVariables method
-  // PowerMock API documentation Section 13 [https://javadoc.io/static/org.mockito/mockito-core/4.6.1/org/mockito/Mockito.html]
-  private static final GitMacheteErrorReportSubmitter reportSubmitter = PowerMockito
-      .spy(new GitMacheteErrorReportSubmitter());
+  private GitMacheteErrorReportSubmitter reportSubmitter;
   private static final LambdaLogger errorReportSubmitterLogger = PowerMockito.mock(LambdaLogger.class);
-  private static String defaultErrorReport;
-  private static String defaultErrorReportWithTemplate;
-  private static String errorReportWithStackTraceAndTitle;
-  private static String systemLineSeparator = "\n";
 
   @BeforeClass
-  @SneakyThrows
   public static void setUpStatic() {
-    defaultErrorReport = IOUtils.resourceToString("/error_report_uri.txt", StandardCharsets.UTF_8).strip();
-    defaultErrorReportWithTemplate = IOUtils.resourceToString("/error_report_uri_with_template.txt",
-        StandardCharsets.UTF_8).strip();
-    errorReportWithStackTraceAndTitle = IOUtils.resourceToString("/error_report_uri_with_stacktrace_title.txt",
-        StandardCharsets.UTF_8).strip();
-  }
-
-  @Before
-  public void setUp() {
     Whitebox.setInternalState(GitMacheteErrorReportSubmitter.class, errorReportSubmitterLogger);
     Whitebox.setInternalState(SystemUtils.class, "OS_NAME", "Mock OS X");
     Whitebox.setInternalState(SystemUtils.class, "OS_VERSION", "Hehe");
 
-    PowerMockito.stub(PowerMockito.method(System.class, "lineSeparator"))
-        .toReturn(systemLineSeparator);
-
     PowerMockito.mockStatic(ApplicationInfo.class);
-    PowerMockito.mockStatic(PluginId.class);
+    PowerMockito.mockStatic(PluginManagerCore.class);
+  }
+
+  @Before
+  public void setUp() {
+    reportSubmitter = new GitMacheteErrorReportSubmitter();
 
     val applicationInfo = PowerMockito.mock(ApplicationInfo.class);
     PowerMockito.when(applicationInfo.getFullApplicationName()).thenReturn("mocked IntelliJ idea");
     PowerMockito.stub(PowerMockito.method(ApplicationInfo.class, "getInstance"))
         .toReturn(applicationInfo);
+
+    val ideaPluginDescriptor = PowerMockito.mock(IdeaPluginDescriptor.class);
+    PowerMockito.when(ideaPluginDescriptor.getVersion()).thenReturn("mock plugin version");
+    PowerMockito.stub(PowerMockito.method(PluginManagerCore.class, "getPlugin"))
+        .toReturn(ideaPluginDescriptor);
   }
 
-  private IdeaLoggingEvent[] getMockEvents(String throwableText) {
-    val customThrowable = new Throwable() {
-      @Override
-      public void printStackTrace(PrintWriter s) {
-        s.print(throwableText);
-      }
-    };
-
-    return new IdeaLoggingEvent[]{
-        new IdeaLoggingEvent(/* message */ "", customThrowable)
-    };
+  private IdeaLoggingEvent getMockEvent(String exceptionMessage, int stackLength) {
+    val stackTraceElement = new StackTraceElement("com.virtuslab.DeclaringClass", "someMethod", "DeclaringClass.java",
+        /* lineNumber */ 1234);
+    val stackTrace = new StackTraceElement[stackLength];
+    Arrays.fill(stackTrace, stackTraceElement);
+    val exception = new RuntimeException(exceptionMessage);
+    exception.setStackTrace(stackTrace);
+    return new IdeaLoggingEvent("some message", exception);
   }
 
-  private String invokeGetReportBody(IdeaLoggingEvent[] events)
-      throws Exception {
-    String reportBody = Whitebox.invokeMethod(reportSubmitter, "getReportBody", events, /* additionalInfo */ null);
-
-    Mockito
-        .verify(errorReportSubmitterLogger,
-            never().description("Reading the resource file with the bug template should NOT throw exceptions"))
-        .error(anyString(), (Throwable) any());
-
-    return reportBody;
+  @SneakyThrows
+  private static String expectedUri(String baseName) {
+    return IOUtils.resourceToString("/expected-error-report-uris/${baseName}.txt", StandardCharsets.UTF_8).replace("\n", "");
   }
 
   @Test
-  public void shouldConstructUriWithTemplate() throws Exception {
-    val events = getMockEvents(/* throwableText */ "");
-    val reportBody = invokeGetReportBody(events);
+  public void shouldConstructUriWithoutStackTrace() throws Exception {
+    val event0 = getMockEvent("exception message", 0);
 
-    URI uriWithReport = Whitebox.invokeMethod(reportSubmitter, "constructNewGitHubIssueUri", events, reportBody);
-    Assert.assertEquals("URI should match default with report body", defaultErrorReportWithTemplate, uriWithReport.toString());
+    URI uri = reportSubmitter.constructNewGitHubIssueUri(new IdeaLoggingEvent[]{event0}, /* additionalInfo */ null);
+    Assert.assertEquals(expectedUri("without_stack_trace"), uri.toString());
   }
 
   @Test
-  public void shouldConstructUriWithNoTemplate() throws Exception {
-    val events = getMockEvents(/* throwableText */ "");
+  public void shouldConstructUriWithStackTrace() throws Exception {
+    val event10 = getMockEvent("exception message", 10);
 
-    URI uriNoReport = Whitebox.invokeMethod(reportSubmitter, "constructNewGitHubIssueUri", events, /* report Body */ null);
-    Assert.assertEquals("URI should match default with NO report body", defaultErrorReport, uriNoReport.toString());
+    URI uri = reportSubmitter.constructNewGitHubIssueUri(new IdeaLoggingEvent[]{event10}, /* additionalInfo */ null);
+    Assert.assertEquals(expectedUri("with_stack_trace"), uri.toString());
   }
 
   @Test
-  public void shouldConstructUriWithTitleAndStackTrace() throws Exception {
-    String throwableText = "Title - top of the stacktrace${systemLineSeparator}stacktrace${systemLineSeparator}stacktrace";
-    val events = getMockEvents(throwableText);
-    val reportBody = invokeGetReportBody(events);
+  public void shouldConstructUriForMultipleEvents() throws Exception {
+    val event0 = getMockEvent("exception message", 0);
+    val event10 = getMockEvent("another exception message", 10);
 
-    URI uri = Whitebox.invokeMethod(reportSubmitter, "constructNewGitHubIssueUri", events, reportBody);
-    Assert.assertEquals("URI should match report template with stacktrace and title", errorReportWithStackTraceAndTitle,
-        uri.toString());
+    URI uri = reportSubmitter.constructNewGitHubIssueUri(new IdeaLoggingEvent[]{event0, event10}, /* additionalInfo */ null);
+    Assert.assertEquals(expectedUri("for_multiple_events"), uri.toString());
   }
 }
