c210d6b18ca8324bff3d13fbf4fdd6d823eec97c
Ina Zimmer <izimmer@virtuslab.com>
2022-09-23 12:10:38 +0200

    Getting the project to build

--- gitCore/jGit/src/test/java/com/virtuslab/gitcore/impl/jgit/GitCoreRepositoryTest.java
+++ gitCore/jGit/src/test/java/com/virtuslab/gitcore/impl/jgit/GitCoreRepositoryTest.java
@@ -1,8 +1,16 @@
 package com.virtuslab.gitcore.impl.jgit;
 
-/*
- * this test seems to be irrelevant. because the method is no longer returning an Option.
-
+import lombok.SneakyThrows;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({GitCoreRepository.class, RevWalk.class})
@@ -15,14 +23,11 @@ public class GitCoreRepositoryTest {
   public void mockGitCoreRepository() {
     PowerMockito.stub(PowerMockito.method(RevWalk.class, "parseCommit")).toThrow(new Exception("Mock"));
     PowerMockito.stub(PowerMockito.method(GitCoreRepository.class, "convertRevisionToObjectId"))
-        .toReturn(Option.some(Whitebox.newInstance(ObjectId.class)));
+        .toReturn(Whitebox.newInstance(ObjectId.class));
   }
 
-    @Test
-    public void shouldContainExceptionsInsideOptionReturningMethods() throws Exception {
-
-      Assert.assertEquals(gitCoreRepository.parseRevision(""), null);
-    }
+  @Test
+  public void shouldContainExceptionsInsideOptionReturningMethods() throws Exception {
+    Assert.assertEquals(gitCoreRepository.parseRevision(""), null);
+  }
 }
-
- */
