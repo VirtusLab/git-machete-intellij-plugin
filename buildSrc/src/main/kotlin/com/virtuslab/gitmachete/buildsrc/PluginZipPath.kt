0d061859ad26a4db739f0ee58c30c4ef185d9e54
LukaszKontowski <lkontowski@virtuslab.com>
2022-09-28 10:52:27 +0200

    remove unused imports

--- buildSrc/src/main/kotlin/com/virtuslab/gitmachete/buildsrc/PluginZipPath.kt
+++ buildSrc/src/main/kotlin/com/virtuslab/gitmachete/buildsrc/PluginZipPath.kt
@@ -1,8 +1,6 @@
 package com.virtuslab.gitmachete.buildsrc
 
 import org.gradle.api.Project
-import java.nio.file.Files
-import java.nio.file.Paths
 
 fun Project.printPluginZipPath() {
     val buildPlugin = tasks.findByPath(":buildPlugin")!!
