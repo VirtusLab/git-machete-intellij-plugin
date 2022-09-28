a80ac998fbacf9af7fddf6bd92d344ee9789fa0f
LukaszKontowski <lkontowski@virtuslab.com>
2022-09-28 11:00:09 +0200

    fix formatting

--- buildSrc/src/main/kotlin/com/virtuslab/gitmachete/buildsrc/PluginZipPath.kt
+++ buildSrc/src/main/kotlin/com/virtuslab/gitmachete/buildsrc/PluginZipPath.kt
@@ -3,6 +3,6 @@ package com.virtuslab.gitmachete.buildsrc
 import org.gradle.api.Project
 
 fun Project.printPluginZipPath() {
-    val buildPlugin = tasks.findByPath(":buildPlugin")!!
-    println(buildPlugin.outputs.files.first().path)
+  val buildPlugin = tasks.findByPath(":buildPlugin")!!
+  println(buildPlugin.outputs.files.first().path)
 }
