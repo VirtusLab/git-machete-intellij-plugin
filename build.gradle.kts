b63155a7a98201839facaa50861035110206a235
LukaszKontowski <lkontowski@virtuslab.com>
2022-09-29 15:16:06 +0200

    replaced first { predicate } with first()

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,8 +60,7 @@ tasks.register("printPluginZipPath") {
 tasks.register("printSignedPluginZipPath") {
   doLast {
     val signPlugin = tasks.findByPath(":signPlugin")!!
-    val signedPluginZipPath = signPlugin.outputs.files.first { file -> file.name.contains("signed") }.path
-    println(signedPluginZipPath)
+    println(signPlugin.outputs.files.first().path)
   }
 }
 
