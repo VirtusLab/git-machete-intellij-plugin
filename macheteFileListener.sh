2a0f73bbb0332c369cd181f04c9aa1b6a9603521
Maciej Gajek <mgajek@virtuslab.com>
2022-10-05 11:48:40 +0100

    Add file listener

---
+++ macheteFileListener.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat .git/machete > macheteChangeLog.txt
+### Set initial time of file
+LTIME=`stat -f %m .git/machete`
+
+while true
+do
+   ATIME=`stat -f %m .git/machete`
+
+   if [[ "$ATIME" != "$LTIME" ]]
+   then
+       date +"%T.%3N" >> macheteChangeLog.txt
+       cat .git/machete >> macheteChangeLog.txt
+       LTIME=$ATIME
+   fi
+done
