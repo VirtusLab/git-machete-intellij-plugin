3beb743d86f1176512b27732444cb7aab849ac88
Pawel Lipski <pawel.p.lipski@gmail.com>
2022-10-06 18:58:01 +0200

    Add javadocs to annotations under `qual/`

--- qual/src/main/java/com/virtuslab/qual/guieffect/UIThreadUnsafe.java
+++ qual/src/main/java/com/virtuslab/qual/guieffect/UIThreadUnsafe.java
@@ -5,9 +5,26 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-// TODO (typetools/checker-framework#3252): replace with proper @Heavyweight
+import org.checkerframework.checker.guieffect.qual.UIEffect;
 
-// Must have runtime retention to be visible to ArchUnit tests.
+/**
+ * Used to mark methods that should NOT be executed on UI thread
+ * (in case of IntelliJ Platform, on Swing's Event Dispatch Thread aka EDT)
+ * to avoid a UI freeze.
+ * These include methods that are heavyweight or blocking: typically, this means accessing disk and/or network.
+ * <p>
+ * Whether the annotation is used correctly is currently NOT enforced during compilation or runtime.
+ * TODO (typetools/checker-framework#3252): replace with proper {@code @Heavyweight} annotation.
+ * As for now, we use a ArchUnit test {@code com.virtuslab.archunit.UIThreadUnsafeMethodInvocationsTestSuite}
+ * to enforce that:
+ * <ol>
+ * <li>methods that access certain known heavyweight methods (like the ones from {@code java.nio}) are marked as {@link UIThreadUnsafe}</li>
+ * <li>methods that access {@link UIThreadUnsafe} methods are themselves marked as {@link UIThreadUnsafe}</li>
+ * <li>no methods are marked as both {@link UIThreadUnsafe} and {@link UIEffect}</li>
+ * </ol>
+ * <p>
+ * This annotation must have runtime retention to be visible to ArchUnit tests.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD, ElementType.CONSTRUCTOR})
 public @interface UIThreadUnsafe {}
--- qual/src/main/java/com/virtuslab/qual/internal/SubtypingTop.java
+++ qual/src/main/java/com/virtuslab/qual/internal/SubtypingTop.java
@@ -8,6 +8,13 @@ import java.lang.annotation.Target;
 import org.checkerframework.framework.qual.DefaultQualifierInHierarchy;
 import org.checkerframework.framework.qual.SubtypeOf;
 
+/**
+ * There needs to be single subtyping hierarchy with single bottom and top annotation.
+ * We could theoretically create a separate hierarchy with a dedicated top and bottom type
+ * for each pair of annotations from {@link com.virtuslab.qual.gitmachete}.* packages,
+ * but then <a href="https://checkerframework.org/manual/#subtyping-checker">Subtyping Checker</a>
+ * would raise an error about multiple top/bottom types.
+ */
 @DefaultQualifierInHierarchy
 @Retention(RetentionPolicy.CLASS)
 @SubtypeOf({})
