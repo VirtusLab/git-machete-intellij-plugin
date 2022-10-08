99bdb6f3c4ebfc8d2804016486615a152034370f
Ishu Thakur <ishuthakur251299@gmail.com>
2022-10-08 23:45:37 +0530

    rolling back the changes in TestProcessUtils

--- testCommon/src/testFixtures/java/com/virtuslab/gitmachete/testcommon/TestProcessUtils.java
+++ testCommon/src/testFixtures/java/com/virtuslab/gitmachete/testcommon/TestProcessUtils.java
@@ -11,30 +11,36 @@ import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 
 public final class TestProcessUtils {
-    private TestProcessUtils() {
+  private TestProcessUtils() {}
+
+  @SneakyThrows
+  public static String runProcessAndReturnStdout(Path workingDirectory, int timeoutSeconds, String... command) {
+    Process process = new ProcessBuilder()
+        .command(command)
+        .directory(workingDirectory.toFile())
+        .start();
+    boolean completed = process.waitFor(timeoutSeconds, TimeUnit.SECONDS);
+
+    String stdout = IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
+
+    String commandRepr = Arrays.toString(command);
+
+    if (!completed || process.exitValue() != 0) {
+      System.out.println("Stdout of ${commandRepr}: ${System.lineSeparator()}");
+      System.out.println(stdout);
+      System.err.println("Stderr of ${commandRepr}: ${System.lineSeparator()");
+      System.err.println(IOUtils.toString(process.getErrorStream(), StandardCharsets.UTF_8));
     }
 
-    @SneakyThrows
-    public static String runProcessAndReturnStdout(Path workingDirectory, int timeoutSeconds, String... command) {
-        Process process = new ProcessBuilder()
-                .command(command)
-                .directory(workingDirectory.toFile())
-                .start();
-        boolean completed = process.waitFor(timeoutSeconds, TimeUnit.SECONDS);
+    Assert.assertTrue("command ${commandRepr} has not completed within ${timeoutSeconds} seconds;", completed);
+    int exitValue = process.exitValue();
+    Assert.assertEquals("command ${commandRepr} has completed with exit code ${exitValue};", 0, exitValue);
 
-        String stdout = IOUtils.toString(process.getInputStream(), StandardCharsets.UTF_8);
+    return stdout;
+  }
 
-        String commandRepr = Arrays.toString(command);
-
-        Assert.assertTrue("Stdout of ${commandRepr}: ${System.lineSeparator()} Stderr of ${commandRepr}: ${System.lineSeparator() command ${commandRepr} has not completed within ${timeoutSeconds} seconds;", stdout, IOUtils.toString(process.getErrorStream(), StandardCharsets.UTF_8), completed);
-        int exitValue = process.exitValue();
-        Assert.assertEquals("command ${commandRepr} has completed with exit code ${exitValue};", 0, exitValue);
-
-        return stdout;
-    }
-
-    public static String runProcessAndReturnStdout(int timeoutSeconds, String... command) {
-        Path currentDir = Paths.get(".").toAbsolutePath().normalize();
-        return runProcessAndReturnStdout(currentDir, timeoutSeconds, command);
-    }
+  public static String runProcessAndReturnStdout(int timeoutSeconds, String... command) {
+    Path currentDir = Paths.get(".").toAbsolutePath().normalize();
+    return runProcessAndReturnStdout(currentDir, timeoutSeconds, command);
+  }
 }
