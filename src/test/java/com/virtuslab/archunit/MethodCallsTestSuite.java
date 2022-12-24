e5db507b9675f81bbb2cbda14786b1739614e357
Pawel Lipski <pawel.p.lipski@gmail.com>
2022-12-24 15:47:15 +0100

    Simplify actions_overriding_onUpdate_should_call_super_onUpdate

--- src/test/java/com/virtuslab/archunit/MethodCallsTestSuite.java
+++ src/test/java/com/virtuslab/archunit/MethodCallsTestSuite.java
@@ -2,52 +2,41 @@ package com.virtuslab.archunit;
 
 import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 
-import com.tngtech.archunit.base.DescribedPredicate;
-import com.tngtech.archunit.core.domain.AccessTarget;
-import com.tngtech.archunit.core.domain.JavaClass;
-import com.tngtech.archunit.core.domain.JavaCodeUnit;
-import com.tngtech.archunit.core.domain.JavaMethodCall;
+import com.tngtech.archunit.core.domain.*;
 import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ConditionEvents;
 import com.tngtech.archunit.lang.SimpleConditionEvent;
+import lombok.val;
 import org.checkerframework.checker.nullness.qual.Nullable;
 import org.junit.Test;
 
 public class MethodCallsTestSuite extends BaseArchUnitTestSuite {
 
   @Test
-  public void actions_overriding_onUpdate_should_call_super_onUpdate() {
-    classes()
-        .that()
-        .areAssignableTo(com.virtuslab.gitmachete.frontend.actions.base.BaseProjectDependentAction.class)
-        .and()
-        .areNotAssignableFrom(com.virtuslab.gitmachete.frontend.actions.base.BaseProjectDependentAction.class)
-        .and(new DescribedPredicate<JavaClass>("override onUpdate method") {
+  public void overridden_onUpdate_methods_should_call_super_onUpdate() {
+    methods().that().haveName("onUpdate")
+        .and().areNotDeclaredIn(com.virtuslab.gitmachete.frontend.actions.base.BaseProjectDependentAction.class)
+        .should(new ArchCondition<>("call onUpdate from the direct superclass") {
+
           @Override
-          public boolean test(JavaClass input) {
-            return input.getMethods().stream().anyMatch(method -> method.getName().equals("onUpdate"));
+          public void check(JavaMethod method, ConditionEvents events) {
+            val superclass = method.getOwner().getSuperclass().orElse(null);
+            if (method.getCallsFromSelf().stream().noneMatch(call -> isACallTo(call, superclass, "onUpdate"))) {
+              String message = "Method ${method.getFullName()} does not call super.onUpdate()";
+              events.add(SimpleConditionEvent.violated(method, message));
+            }
+          }
+
+          private boolean isACallTo(JavaCall<?> call, JavaType methodOwner, String methodName) {
+            return call.getTarget().getOwner().equals(methodOwner) && call.getTarget().getName().equals(methodName);
           }
         })
-        .should()
-        .callMethodWhere(
-            new DescribedPredicate<JavaMethodCall>("name is onUpdate and owner is the direct superclass") {
-              @Override
-              public boolean test(JavaMethodCall input) {
-                JavaCodeUnit origin = input.getOrigin(); // where is the method called from?
-                AccessTarget.MethodCallTarget target = input.getTarget(); // where is the method declared?
-
-                if (origin.getName().equals("onUpdate") && target.getName().equals("onUpdate")) {
-                  return target.getOwner().equals(origin.getOwner().getSuperclass().orElse(null));
-                }
-                return false;
-              }
-            })
         .check(importedClasses);
 
   }
 
   @Test
-  public void methods_calling_MessageBusConnection_subscribe_must_later_call_Disposer_register_too() {
+  public void code_units_calling_MessageBusConnection_subscribe_must_later_call_Disposer_register_too() {
     String subscribeMethodFullName = "com.intellij.util.messages.MessageBusConnection.subscribe(com.intellij.util.messages.Topic, java.lang.Object)";
     String registerMethodFullName = "com.intellij.openapi.util.Disposer.register(com.intellij.openapi.Disposable, com.intellij.openapi.Disposable)";
 
@@ -68,13 +57,12 @@ public class MethodCallsTestSuite extends BaseArchUnitTestSuite {
           }
         }
       }
+
+      private @Nullable JavaMethodCall findFirstCallTo(JavaCodeUnit sourceCodeUnit, String targetMethodFullName) {
+        return sourceCodeUnit.getMethodCallsFromSelf().stream()
+            .filter(call -> call.getTarget().getFullName().equals(targetMethodFullName)).findFirst()
+            .orElse(null);
+      }
     }).check(importedClasses);
   }
-
-  private static @Nullable JavaMethodCall findFirstCallTo(JavaCodeUnit sourceCodeUnit, String targetMethodFullName) {
-    return sourceCodeUnit.getMethodCallsFromSelf().stream()
-        .filter(call -> call.getTarget().getFullName().equals(targetMethodFullName)).findFirst()
-        .orElse(null);
-  }
-
 }
