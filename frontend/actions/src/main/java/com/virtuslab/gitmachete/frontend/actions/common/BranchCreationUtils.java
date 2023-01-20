58eaf7b4b3c776b048fb2c3e62ba0e0b2dbde4e0
mkondratek <mik.kondratek@gmail.com>
2023-01-20 12:22:10 +0100

    Add rename action (closes #1183) - reapply RenameBackgroundable

---
+++ frontend/actions/src/main/java/com/virtuslab/gitmachete/frontend/actions/common/BranchCreationUtils.java
@@ -0,0 +1,48 @@
+package com.virtuslab.gitmachete.frontend.actions.common;
+
+import static com.virtuslab.gitmachete.frontend.resourcebundles.GitMacheteBundle.getString;
+
+import com.intellij.openapi.vcs.VcsNotifier;
+import git4idea.GitLocalBranch;
+import git4idea.repo.GitRepository;
+import lombok.experimental.ExtensionMethod;
+import lombok.val;
+import org.checkerframework.checker.nullness.qual.Nullable;
+
+import com.virtuslab.gitmachete.frontend.resourcebundles.GitMacheteBundle;
+import com.virtuslab.qual.guieffect.UIThreadUnsafe;
+
+@ExtensionMethod(GitMacheteBundle.class)
+public final class BranchCreationUtils {
+
+  private BranchCreationUtils() {}
+
+  @UIThreadUnsafe
+  public static @Nullable GitLocalBranch findLocalBranch(GitRepository gitRepository, String branchName) {
+    return gitRepository.getBranches().findLocalBranch(branchName);
+  }
+
+  @UIThreadUnsafe
+  public static void waitForCreationOfLocalBranch(GitRepository gitRepository, String branchName) {
+    try {
+      // Usually just 3 attempts are enough
+      val MAX_SLEEP_DURATION = 8192;
+      var sleepDuration = 64;
+      while (findLocalBranch(gitRepository, branchName) == null && sleepDuration <= MAX_SLEEP_DURATION) {
+        Thread.sleep(sleepDuration);
+        sleepDuration *= 2;
+      }
+    } catch (InterruptedException e) {
+      VcsNotifier.getInstance(gitRepository.getProject()).notifyWeakError(/* displayId */ null,
+          /* title */ "",
+          getString("action.GitMachete.BaseSlideInBackgroundable.notification.message.wait-interrupted")
+              .fmt(branchName));
+    }
+
+    if (findLocalBranch(gitRepository, branchName) == null) {
+      VcsNotifier.getInstance(gitRepository.getProject()).notifyWeakError(/* displayId */ null,
+          /* title */ "",
+          getString("action.GitMachete.BaseSlideInBackgroundable.notification.message.timeout").fmt(branchName));
+    }
+  }
+}
