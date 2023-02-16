13a64599dc7e612c6acdd79d904a5633328aeecd
Pawel Lipski <pawel.p.lipski@gmail.com>
2023-02-16 18:37:32 +0100

    Make traverse actually perform a pull/fetch for branches behind its remote

--- frontend/actions/src/main/java/com/virtuslab/gitmachete/frontend/actions/base/BasePullAction.java
+++ frontend/actions/src/main/java/com/virtuslab/gitmachete/frontend/actions/base/BasePullAction.java
@@ -1,23 +1,16 @@
 package com.virtuslab.gitmachete.frontend.actions.base;
 
-import static com.virtuslab.gitmachete.frontend.actions.common.FetchUpToDateTimeoutStatus.FETCH_ALL_UP_TO_DATE_TIMEOUT_AS_STRING;
-import static com.virtuslab.gitmachete.frontend.resourcebundles.GitMacheteBundle.getNonHtmlString;
 import static com.virtuslab.gitmachete.frontend.resourcebundles.GitMacheteBundle.getString;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
-import git4idea.repo.GitRepository;
 import io.vavr.collection.List;
 import lombok.experimental.ExtensionMethod;
 import lombok.val;
 import org.checkerframework.checker.guieffect.qual.UIEffect;
 import org.checkerframework.checker.i18nformatter.qual.I18nFormat;
 
-import com.virtuslab.gitmachete.backend.api.IRemoteTrackingBranchReference;
 import com.virtuslab.gitmachete.backend.api.SyncToRemoteStatus;
-import com.virtuslab.gitmachete.frontend.actions.backgroundables.FastForwardMergeBackgroundable;
-import com.virtuslab.gitmachete.frontend.actions.backgroundables.FetchBackgroundable;
-import com.virtuslab.gitmachete.frontend.actions.common.FetchUpToDateTimeoutStatus;
-import com.virtuslab.gitmachete.frontend.actions.common.MergeProps;
+import com.virtuslab.gitmachete.frontend.actions.common.Pull;
 import com.virtuslab.gitmachete.frontend.actions.expectedkeys.IExpectsKeyGitMacheteRepository;
 import com.virtuslab.gitmachete.frontend.resourcebundles.GitMacheteBundle;
 import com.virtuslab.qual.async.ContinuesInBackground;
@@ -84,52 +77,8 @@ public abstract class BasePullAction extends BaseGitMacheteRepositoryReadyAction
         return;
       }
 
-      val mergeProps = new MergeProps(
-          /* movingBranchName */ localBranch,
-          /* stayingBranchName */ remoteBranch);
-
-      val isUpToDate = FetchUpToDateTimeoutStatus.isUpToDate(gitRepository);
-      val fetchNotificationPrefix = isUpToDate
-          ? getNonHtmlString("action.GitMachete.BasePullAction.notification.prefix.no-fetch-perform")
-              .fmt(FETCH_ALL_UP_TO_DATE_TIMEOUT_AS_STRING)
-          : getNonHtmlString("action.GitMachete.BasePullAction.notification.prefix.fetch-perform");
-      val fetchNotificationTextPrefix = fetchNotificationPrefix + (fetchNotificationPrefix.isEmpty() ? "" : " ");
-      val fastForwardBackgroundable = new FastForwardMergeBackgroundable(gitRepository, mergeProps,
-          fetchNotificationTextPrefix);
-
-      if (isUpToDate) {
-        fastForwardBackgroundable.queue();
-      } else {
-        updateRepositoryFetchBackgroundable(gitRepository, remoteBranch,
-            /* onSuccessRunnable */ () -> fastForwardBackgroundable.queue());
-      }
+      new Pull(gitRepository, localBranch, remoteBranch).run();
     }
   }
 
-  @ContinuesInBackground
-  private void updateRepositoryFetchBackgroundable(
-      GitRepository gitRepository,
-      IRemoteTrackingBranchReference remoteBranch,
-      Runnable onSuccessRunnable) {
-    val remoteName = remoteBranch.getRemoteName();
-
-    String taskTitle = getString("action.GitMachete.BasePullAction.task-title");
-
-    new FetchBackgroundable(
-        gitRepository,
-        remoteName,
-        /* refspec */ null, // let's use the default refspec for the given remote, as defined in git config
-        taskTitle,
-        getNonHtmlString("action.GitMachete.BasePullAction.notification.title.pull-fail").fmt(remoteBranch.getName()),
-        getString("action.GitMachete.BasePullAction.notification.title.pull-success.HTML").fmt(remoteBranch.getName())) {
-
-      @Override
-      @UIEffect
-      public void onSuccess() {
-        String repoName = gitRepository.getRoot().getName();
-        FetchUpToDateTimeoutStatus.update(repoName);
-        onSuccessRunnable.run();
-      }
-    }.queue();
-  }
 }
