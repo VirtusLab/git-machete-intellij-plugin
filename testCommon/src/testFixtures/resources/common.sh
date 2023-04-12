7a44a6a740adfbc907684f1feddb314362f5e16e
Paweł Lipski <pawel.p.lipski@gmail.com>
2023-04-12 12:56:16 +0200

    Test handling of failures in `machete-status-branch` hook (#1643)
    * Test handling of failures in `machete-status-branch` hook
    
    * Implement `machete-post-slide-out` hook - 1st round of fixes

--- testCommon/src/testFixtures/resources/common.sh
+++ testCommon/src/testFixtures/resources/common.sh
@@ -3,7 +3,13 @@
 status_branch_hook=$(cat <<'EOF'
 #!/usr/bin/env bash
 branch=$1
+# For machete-status-branch hook, let's just use a command whose output would differ between the branches.
 file_count=$(git ls-tree $branch | wc -l | sed 's/^ *//')
+if [[ $branch = master ]]; then
+  # To test handling of failures (both stdout and stderr should be ignored)
+  echo Error | tee /dev/stderr
+  exit 1
+fi
 echo "<$file_count files>"
 EOF
 )
