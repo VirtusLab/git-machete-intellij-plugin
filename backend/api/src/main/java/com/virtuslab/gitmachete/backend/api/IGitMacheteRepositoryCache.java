9b5c9c60e7e9cb1aad7c95db8a5d45bb6cc8e733
Paweł Lipski <pawel.p.lipski@gmail.com>
2025-08-01 16:29:33 +0200

    Bump Gradle to v9.0.0 (#2105)

--- backend/api/src/main/java/com/virtuslab/gitmachete/backend/api/IGitMacheteRepositoryCache.java
+++ backend/api/src/main/java/com/virtuslab/gitmachete/backend/api/IGitMacheteRepositoryCache.java
@@ -7,7 +7,7 @@ import com.virtuslab.qual.guieffect.UIThreadUnsafe;
 /** Each implementing class must have a public parameterless constructor. */
 public interface IGitMacheteRepositoryCache {
   @FunctionalInterface
-  public interface Injector {
+  interface Injector {
     <T> T inject(Class<T> clazz);
   }
 
